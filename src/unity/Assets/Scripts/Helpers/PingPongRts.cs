f498b50edcb4775004c4dae9e621d95e17f4bf23
huwb <huw.bowles@gmail.com>
2017-09-09 18:04:58 +0100

    Debug display of shape textures

--- src/unity/Assets/Scripts/Helpers/PingPongRts.cs
+++ src/unity/Assets/Scripts/Helpers/PingPongRts.cs
@@ -35,5 +35,24 @@ public class PingPongRts : MonoBehaviour
         }
     }
 
+    void OnGUI()
+    {
+        // draw source textures to screen
+        if( sourceThisFrame )
+        {
+            int ind = ShapeWaveSim.GetShapeCamIndex( Cam );
+            if( ind < 0 ) return;
+
+            float b = 7f;
+            float h = Screen.height/(float)OceanResearch.OceanRenderer.Instance.Builder._shapeCameras.Length, w = h + b;
+            float x = Screen.width - w, y = ind * h;
+
+            GUI.color = Color.black * 0.7f;
+            GUI.DrawTexture( new Rect( x, y, w, h ), Texture2D.whiteTexture );
+            GUI.color = Color.white;
+            GUI.DrawTexture( new Rect( x + b, y + b / 2f, h - b, h - b ), sourceThisFrame );
+        }
+    }
+
     Camera _cam; Camera Cam { get { return _cam != null ? _cam : (_cam = GetComponent<Camera>()); } }
 }

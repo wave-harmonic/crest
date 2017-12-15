b5ca42cefa9db911dfb743eb2c553500c9aa590b
huwb <huw.bowles@gmail.com>
2017-12-15 22:41:22 +0000

    Disable sphere offset, which would move the lods around (especially the low lods) and scrub out large portions of the simulation results :(. This results in slightly less detail, but now follows 1/z density, and the patches behind the viewer will be culled.

---
+++ src/unity/Assets/Scripts/Placement/PlaceUnderTransform.cs
@@ -0,0 +1,25 @@
+﻿// This file is subject to the MIT License as seen in the root of this folder structure (LICENSE)
+
+using UnityEngine;
+
+namespace OceanResearch.Placement
+{
+    /// <summary>
+    /// Positions this GameObject at a flatland position which shares XZ coordinates with a target transform.
+    /// </summary>
+    public class PlaceUnderTransform : MonoBehaviour
+    {
+        public Transform _viewpoint;
+
+        // the script execution order ensures this executes early in the ocean late update
+        void LateUpdate()
+        {
+            Vector3 pos = _viewpoint.position;
+
+            // maintain y coordinate - sea level
+            pos.y = transform.position.y;
+
+            transform.position = pos;
+        }
+    }
+}

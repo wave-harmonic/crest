6ebf718315498d5c1996901463870653caaf0636
huwb <huw.bowles@gmail.com>
2018-02-17 22:32:50 +0000

    No more cached positions - just randomly sample N points on screen each frame to detect choppy parts.

--- src/unity/Assets/Scripts/OceanFX.cs
+++ src/unity/Assets/Scripts/OceanFX.cs
@@ -8,28 +8,14 @@ namespace Crest
         public int _samplesPerFrame = 32;
 
         ShapeGerstnerBatched _gerstner;
-        public ParticleSystem _particles;
-
-        Vector3[] basePositions;
-        float[] moveTimes;
+        public ParticleSystem _particlePrefab;
 
         void Start()
         {
             _gerstner = GetComponent<ShapeGerstnerBatched>();
-
-            basePositions = new Vector3[_samplesPerFrame];
-            moveTimes = new float[_samplesPerFrame];
-
-            for (int i = 0; i < _samplesPerFrame; i++)
-            {
-                basePositions[i] = Camera.main.transform.position + 50f * Random.insideUnitSphere;
-                basePositions[i].y = 0f;
-                moveTimes[i] = 0f;
-            }
         }
 
         public float _emitThresh = 0.7f;
-        public float _maxTimeStationary = 0.25f;
         public float _minYVel = 1f;
         public bool _showPoints = false;
 
@@ -37,19 +23,26 @@ namespace Crest
         {
             for (int i = 0; i < _samplesPerFrame; i++)
             {
-                float ss = 0.5f;
+                // this tests on screen positions undisplaced positions. it might be interesting to look a bit outside frustum.
+                var randomPos = new Vector3(Random.value * Screen.width, Random.value * Screen.height, 0f);
+                var ray = Camera.main.ScreenPointToRay(randomPos);
+                // if close to or above horizon, just skip this sample. another option would be to negate the y component of the direction
+                if (ray.direction.y > -0.01f)
+                    continue;
 
-                Vector3 disp = _gerstner.GetDisplacement(basePositions[i], 0f);
-
+                var queryPos = ray.origin + ray.direction * -ray.origin.y / ray.direction.y;
+                var disp = _gerstner.GetDisplacement(queryPos, 0f);
                 if (_showPoints)
                 {
-                    Debug.DrawLine(basePositions[i], basePositions[i] + disp);
+                    Debug.DrawLine(queryPos, queryPos + disp);
                 }
+
+                float ss = 0.5f;
 
                 Vector3 disp_x = Vector3.right * ss;
                 Vector3 disp_z = Vector3.forward * ss;
-                disp_x += _gerstner.GetDisplacement(disp_x + basePositions[i], 0f);
-                disp_z += _gerstner.GetDisplacement(disp_z + basePositions[i], 0f);
+                disp_x += _gerstner.GetDisplacement(disp_x + queryPos, 0f);
+                disp_z += _gerstner.GetDisplacement(disp_z + queryPos, 0f);
 
                 float dux = disp_x.x - disp.x;
                 float duy = disp_x.z - disp.z;
@@ -62,42 +55,21 @@ namespace Crest
                 float det = (dux * duw - duy * duz) / (ss * ss);
                 det = Mathf.InverseLerp(1.6f, 0f, det);
 
+                // compute velocity
                 float dt = 1f / 60f;
                 //Vector3 disp_tm = _gerstner.GetDisplacement(pt, -dt);
-                Vector3 disp_tp = _gerstner.GetDisplacement(basePositions[i], dt);
+                Vector3 disp_tp = _gerstner.GetDisplacement(queryPos, dt);
                 Vector3 vel = (disp_tp - disp) / dt;
                 //float yacc = (disp_tm.y + disp_tp.y - 2f * disp.y) / (dt * dt);
 
                 if (det >= _emitThresh && vel.y >= _minYVel)
                 {
-                    var ps = Instantiate(_particles.transform);
-                    ps.position = basePositions[i] + disp;
+                    var ps = Instantiate(_particlePrefab.transform);
+                    ps.position = queryPos + disp;
                     ps.LookAt(ps.position + Vector3.Lerp(Vector3.up, vel, Random.value));
-                    ps.GetComponent<TrackOceanSurface>()._basePosition = basePositions[i];
-
-                    MoveRand(i);
-                }
-                else if (TimeSinceMove(i) > _maxTimeStationary)
-                {
-                    MoveRand(i);
+                    ps.GetComponent<TrackOceanSurface>()._basePosition = queryPos;
                 }
             }
-        }
-
-        float TimeSinceMove(int i)
-        {
-            return Time.time - moveTimes[i];
-        }
-
-
-        void MoveRand(int i)
-        {
-            float radForwards = 80f;
-            float rad = 40f;
-            basePositions[i] = Camera.main.transform.position + Random.value * radForwards * Camera.main.transform.forward + 2f * (Random.value - 0.5f) * rad * Camera.main.transform.right;
-            basePositions[i].y = 0f;
-
-            moveTimes[i] = Time.time;
         }
     }
 }

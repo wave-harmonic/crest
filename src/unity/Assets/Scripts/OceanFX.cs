e696109e37fa3450744ecd801dd2e72f1db0ae58
huwb <huw.bowles@gmail.com>
2018-02-17 22:13:40 +0000

    No more marker objects for fx locations

--- src/unity/Assets/Scripts/OceanFX.cs
+++ src/unity/Assets/Scripts/OceanFX.cs
@@ -4,7 +4,9 @@ namespace Crest
 {
     public class OceanFX : MonoBehaviour
     {
-        public Transform[] _markers;
+        [Range(0, 63)]
+        public int _samplesPerFrame = 32;
+
         ShapeGerstnerBatched _gerstner;
         public ParticleSystem _particles;
 
@@ -15,12 +17,14 @@ namespace Crest
         {
             _gerstner = GetComponent<ShapeGerstnerBatched>();
 
-            basePositions = new Vector3[_markers.Length];
-            moveTimes = new float[_markers.Length];
+            basePositions = new Vector3[_samplesPerFrame];
+            moveTimes = new float[_samplesPerFrame];
 
-            for (int i = 0; i < _markers.Length; i++)
+            for (int i = 0; i < _samplesPerFrame; i++)
             {
-                basePositions[i] = _markers[i].position;
+                basePositions[i] = Camera.main.transform.position + 50f * Random.insideUnitSphere;
+                basePositions[i].y = 0f;
+                moveTimes[i] = 0f;
             }
         }
 
@@ -31,26 +35,21 @@ namespace Crest
 
         void Update()
         {
-            for (int i = 0; i < _markers.Length; i++)
+            for (int i = 0; i < _samplesPerFrame; i++)
             {
-                Transform marker = _markers[i];
-
                 float ss = 0.5f;
 
-                Vector3 pt = basePositions[i];
-
-                Vector3 disp = _gerstner.GetDisplacement(pt, 0f);
-                marker.position = pt + disp;
+                Vector3 disp = _gerstner.GetDisplacement(basePositions[i], 0f);
 
                 if (_showPoints)
                 {
-                    Debug.DrawLine(pt, pt + disp);
+                    Debug.DrawLine(basePositions[i], basePositions[i] + disp);
                 }
 
                 Vector3 disp_x = Vector3.right * ss;
                 Vector3 disp_z = Vector3.forward * ss;
-                disp_x += _gerstner.GetDisplacement(disp_x + pt, 0f);
-                disp_z += _gerstner.GetDisplacement(disp_z + pt, 0f);
+                disp_x += _gerstner.GetDisplacement(disp_x + basePositions[i], 0f);
+                disp_z += _gerstner.GetDisplacement(disp_z + basePositions[i], 0f);
 
                 float dux = disp_x.x - disp.x;
                 float duy = disp_x.z - disp.z;
@@ -60,21 +59,19 @@ namespace Crest
                 // > 1: Stretch
                 // < 1: Squash
                 // < 0: Overlap
-                //float4 du = float4(disp_x.xz, disp_z.xz) - disp.xzxz;
-                //Vector4 du = new Vector4()
                 float det = (dux * duw - duy * duz) / (ss * ss);
                 det = Mathf.InverseLerp(1.6f, 0f, det);
 
                 float dt = 1f / 60f;
                 //Vector3 disp_tm = _gerstner.GetDisplacement(pt, -dt);
-                Vector3 disp_tp = _gerstner.GetDisplacement(pt, dt);
+                Vector3 disp_tp = _gerstner.GetDisplacement(basePositions[i], dt);
                 Vector3 vel = (disp_tp - disp) / dt;
                 //float yacc = (disp_tm.y + disp_tp.y - 2f * disp.y) / (dt * dt);
 
                 if (det >= _emitThresh && vel.y >= _minYVel)
                 {
                     var ps = Instantiate(_particles.transform);
-                    ps.position = marker.transform.position;
+                    ps.position = basePositions[i] + disp;
                     ps.LookAt(ps.position + Vector3.Lerp(Vector3.up, vel, Random.value));
                     ps.GetComponent<TrackOceanSurface>()._basePosition = basePositions[i];
 
@@ -84,9 +81,6 @@ namespace Crest
                 {
                     MoveRand(i);
                 }
-
-                Renderer rend = marker.GetComponent<Renderer>();
-                rend.enabled = _showPoints;
             }
         }
 

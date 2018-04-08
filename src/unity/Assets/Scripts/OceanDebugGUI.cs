f0584306032ca22f554d3135617e64aa77db0942
Huw Bowles <huw.bowles@gmail.com>
2018-04-08 22:37:42 +0100

    Separate debug gui controls from shape target visualisation in case one is wanted without the other.

--- src/unity/Assets/Scripts/OceanDebugGUI.cs
+++ src/unity/Assets/Scripts/OceanDebugGUI.cs
@@ -25,45 +25,43 @@ namespace Crest
 
         void OnGUI()
         {
-            if(!_guiVisible)
-            {
-                return;
-            }
-
             Color bkp = GUI.color;
 
-            GUI.skin.toggle.normal.textColor = Color.white;
-            GUI.skin.label.normal.textColor = Color.white;
-
-            float x = 5f, y = 0f;
-            float w = _leftPanelWidth - 2f * x, h = 25f;
-
-            GUI.color = Color.black * 0.7f;
-            GUI.DrawTexture( new Rect( 0, 0, w + 2f * x, Screen.height ), Texture2D.whiteTexture );
-            GUI.color = Color.white;
-
-            RenderWireFrame._wireFrame = GUI.Toggle(new Rect(x, y, w, h), RenderWireFrame._wireFrame, "Wireframe"); y += h;
-
-            OceanRenderer.Instance._freezeTime = GUI.Toggle(new Rect(x, y, w, h), OceanRenderer.Instance._freezeTime, "Freeze waves"); y += h;
-
-            GUI.Label(new Rect(x, y, w, h), string.Format("Chop: {0}", OceanRenderer.Instance._chop.ToString("0.00"))); y += h;
-            OceanRenderer.Instance._chop = GUI.HorizontalSlider(new Rect(x, y, w, h), OceanRenderer.Instance._chop, 0f, 2f); y += h;
-
-            OceanRenderer.Instance._enableSmoothLOD = GUI.Toggle( new Rect( x, y, w, h ), OceanRenderer.Instance._enableSmoothLOD, "Enable smooth LOD" ); y += h;
-
-            GUI.Label( new Rect( x, y, w, h ), string.Format( "Min verts per wave: {0}", OceanRenderer.Instance._minTexelsPerWave.ToString( "0.00" ) ) ); y += h;
-            OceanRenderer.Instance._minTexelsPerWave = GUI.HorizontalSlider( new Rect( x, y, w, h ), OceanRenderer.Instance._minTexelsPerWave, 0, 15 ); y += h;
-
-            _showSimTargets = GUI.Toggle( new Rect( x, y, w, h ), _showSimTargets, "Show shape data" ); y += h;
-            WaveDataCam._shapeCombinePass = GUI.Toggle(new Rect(x, y, w, h), WaveDataCam._shapeCombinePass, "Shape combine pass"); y += h;
-
-            OceanRenderer._acceptLargeWavelengthsInLastLOD = GUI.Toggle(new Rect(x, y, w, h), OceanRenderer._acceptLargeWavelengthsInLastLOD, "Large waves in last LOD"); y += h;
-
-            if (GUI.Button(new Rect(x, y, w, h), "Hide GUI (G)"))
+            if (_guiVisible)
             {
-                ToggleGUI();
+                GUI.skin.toggle.normal.textColor = Color.white;
+                GUI.skin.label.normal.textColor = Color.white;
+
+                float x = 5f, y = 0f;
+                float w = _leftPanelWidth - 2f * x, h = 25f;
+
+                GUI.color = Color.black * 0.7f;
+                GUI.DrawTexture(new Rect(0, 0, w + 2f * x, Screen.height), Texture2D.whiteTexture);
+                GUI.color = Color.white;
+
+                RenderWireFrame._wireFrame = GUI.Toggle(new Rect(x, y, w, h), RenderWireFrame._wireFrame, "Wireframe"); y += h;
+
+                OceanRenderer.Instance._freezeTime = GUI.Toggle(new Rect(x, y, w, h), OceanRenderer.Instance._freezeTime, "Freeze waves"); y += h;
+
+                GUI.Label(new Rect(x, y, w, h), string.Format("Chop: {0}", OceanRenderer.Instance._chop.ToString("0.00"))); y += h;
+                OceanRenderer.Instance._chop = GUI.HorizontalSlider(new Rect(x, y, w, h), OceanRenderer.Instance._chop, 0f, 2f); y += h;
+
+                OceanRenderer.Instance._enableSmoothLOD = GUI.Toggle(new Rect(x, y, w, h), OceanRenderer.Instance._enableSmoothLOD, "Enable smooth LOD"); y += h;
+
+                GUI.Label(new Rect(x, y, w, h), string.Format("Min verts per wave: {0}", OceanRenderer.Instance._minTexelsPerWave.ToString("0.00"))); y += h;
+                OceanRenderer.Instance._minTexelsPerWave = GUI.HorizontalSlider(new Rect(x, y, w, h), OceanRenderer.Instance._minTexelsPerWave, 0, 15); y += h;
+
+                _showSimTargets = GUI.Toggle(new Rect(x, y, w, h), _showSimTargets, "Show shape data"); y += h;
+                WaveDataCam._shapeCombinePass = GUI.Toggle(new Rect(x, y, w, h), WaveDataCam._shapeCombinePass, "Shape combine pass"); y += h;
+
+                OceanRenderer._acceptLargeWavelengthsInLastLOD = GUI.Toggle(new Rect(x, y, w, h), OceanRenderer._acceptLargeWavelengthsInLastLOD, "Large waves in last LOD"); y += h;
+
+                if (GUI.Button(new Rect(x, y, w, h), "Hide GUI (G)"))
+                {
+                    ToggleGUI();
+                }
+                y += h;
             }
-            y += h;
 
             // draw source textures to screen
             if ( _showSimTargets )

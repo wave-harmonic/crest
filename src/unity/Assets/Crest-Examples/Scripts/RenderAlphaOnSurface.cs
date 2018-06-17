1728555033a0faaccff0fe2ac039c0105f24495f
Huw Bowles <huw.bowles@gmail.com>
2018-06-17 22:54:03 +0100

    Shader for rendering alpha stuck to ocean surface

---
+++ src/unity/Assets/Crest-Examples/Scripts/RenderAlphaOnSurface.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using Crest;
+
+public class RenderAlphaOnSurface : MonoBehaviour
+{
+    Material _mat;
+
+	void Start()
+    {
+        _mat = GetComponent<Renderer>().material;
+    }
+
+    private void LateUpdate()
+    {
+        // find which lod this object is overlapping
+        var rect = new Rect(transform.position.x, transform.position.z, 0f, 0f);
+        var idx = WaveDataCam.SuggestCollisionLOD(rect);
+        
+        if (idx > -1)
+        {
+            var pwm = new PropertyWrapperMaterial(_mat);
+            var wdcs = OceanRenderer.Instance.Builder._shapeWDCs;
+            wdcs[idx].ApplyMaterialParams(0, pwm);
+            int idx1 = Mathf.Min(idx + 1, wdcs.Length - 1);
+            wdcs[idx1].ApplyMaterialParams(1, pwm);
+
+            // blend LOD 0 shape in/out to avoid pop, if the ocean might scale up later (it is smaller than its maximum scale)
+            bool needToBlendOutShape = idx == 0 && OceanRenderer.Instance.ScaleCouldIncrease;
+            float meshScaleLerp = needToBlendOutShape ? OceanRenderer.Instance.ViewerAltitudeLevelAlpha : 0f;
+
+            // blend furthest normals scale in/out to avoid pop, if scale could reduce
+            bool needToBlendOutNormals = idx == wdcs.Length - 1 && OceanRenderer.Instance.ScaleCouldDecrease;
+            float farNormalsWeight = needToBlendOutNormals ? OceanRenderer.Instance.ViewerAltitudeLevelAlpha : 1f;
+            pwm.SetVector("_InstanceData", new Vector4(meshScaleLerp, farNormalsWeight, idx));
+        }
+    }
+}

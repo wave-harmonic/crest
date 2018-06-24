7a7afb0946468aa368f7035e2407868cdd56b9e8
Huw Bowles <huw.bowles@gmail.com>
2018-06-24 23:03:19 +0100

    Expanded coll provider API to support boat physics.

--- src/unity/Assets/Crest/Scripts/Collision/CollProviderDispTexs.cs
+++ src/unity/Assets/Crest/Scripts/Collision/CollProviderDispTexs.cs
@@ -45,5 +45,52 @@ namespace Crest
             height = OceanRenderer.Instance.Builder._shapeWDCs[_areaLod].CollData.GetHeight(ref worldPos);
             return true;
         }
+
+        public bool SampleNormal(ref Vector3 undisplacedWorldPos, ref Vector3 normal)
+        {
+            return SampleNormal(ref undisplacedWorldPos, ref normal, 0f);
+        }
+        public bool SampleNormal(ref Vector3 undisplacedWorldPos, ref Vector3 normal, float minSpatialLength)
+        {
+            // select lod. this now has a 1 texel buffer, so the finite differences below should all be valid.
+            PrewarmForSamplingArea(new Rect(undisplacedWorldPos.x, undisplacedWorldPos.z, 0f, 0f), minSpatialLength);
+
+            float gridSize = OceanRenderer.Instance.Builder._shapeWDCs[_areaLod]._renderData._texelWidth;
+
+            Vector3 dispCenter = Vector3.zero;
+            if (!SampleDisplacementInArea(ref undisplacedWorldPos, ref dispCenter)) return false;
+            Vector3 undisplacedWorldPosX = undisplacedWorldPos + Vector3.right * gridSize;
+            Vector3 dispX = Vector3.zero;
+            if (!SampleDisplacementInArea(ref undisplacedWorldPosX, ref dispX)) return false;
+            Vector3 undisplacedWorldPosZ = undisplacedWorldPos + Vector3.forward * gridSize;
+            Vector3 dispZ = Vector3.zero;
+            if (!SampleDisplacementInArea(ref undisplacedWorldPosZ, ref dispZ)) return false;
+
+            normal = Vector3.Cross(dispZ + Vector3.forward * gridSize - dispCenter, dispX + Vector3.right * gridSize - dispCenter).normalized;
+
+            return true;
+        }
+
+        public bool ComputeUndisplacedPosition(ref Vector3 worldPos, ref Vector3 undisplacedWorldPos)
+        {
+            // fpi - guess should converge to location that displaces to the target position
+            Vector3 guess = worldPos;
+            // 2 iterations was enough to get very close when chop = 1, added 2 more which should be
+            // sufficient for most applications. for high chop values or really stormy conditions there may
+            // be some error here. one could also terminate iteration based on the size of the error, this is
+            // worth trying but is left as future work for now.
+            Vector3 disp = Vector3.zero;
+            for (int i = 0; i < 4 && SampleDisplacement(ref guess, ref disp); i++)
+            {
+                Vector3 error = guess + disp - worldPos;
+                guess.x -= error.x;
+                guess.z -= error.z;
+            }
+
+            undisplacedWorldPos = guess;
+            undisplacedWorldPos.y = OceanRenderer.Instance.SeaLevel;
+
+            return true;
+        }
     }
 }

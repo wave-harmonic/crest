060a75e169c73efdbd7047b53915810d4459fd54
Huw Bowles <huw.bowles@gmail.com>
2018-08-15 15:48:48 +0600

    #44 - added helper script to diagnose dt issues

---
+++ src/unity/Assets/Crest-Examples/Scripts/Sleeper.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Sleeper : MonoBehaviour
+{
+    public int _sleepMs = 0;
+    public bool _jitter = false;
+
+	void Update()
+    {
+        int sleep = _jitter ? (int)(Random.value * _sleepMs) : _sleepMs;
+        System.Threading.Thread.Sleep(sleep);
+	}
+}

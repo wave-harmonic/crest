0189ae07b7278adc085d30d131aa2e774f084b7d
Huw Bowles <huw.bowles@gmail.com>
2018-09-11 22:08:48 +0100

    #25 - Ping pong data.
    Cleared buffer at the right time to solve the issue where cmdbuf does not run and shadows freeze.
    
    Remaining problem is the weird shapes at the sides of the screen..

--- src/unity/Assets/Crest/Scripts/LodData/LodDataShadow.cs
+++ src/unity/Assets/Crest/Scripts/LodData/LodDataShadow.cs
@@ -10,9 +10,10 @@ namespace Crest
         public override void UseSettings(SimSettingsBase settings) { }
         public override RenderTextureFormat TextureFormat { get { return RenderTextureFormat.RHalf; } }
         public override CameraClearFlags CamClearFlags { get { return CameraClearFlags.Color; } }
-        public override RenderTexture DataTexture { get { return _shadowData; } }
+        public override RenderTexture DataTexture { get { return _shadowData[_rtIndex]; } }
 
-        RenderTexture _shadowData;
+        int _rtIndex = 1;
+        RenderTexture[] _shadowData = new RenderTexture[2];
         CommandBuffer _bufCopyShadowMap = null;
         Light _mainLight;
         Material _renderMaterial;
@@ -22,11 +23,14 @@ namespace Crest
             base.Start();
 
             int res = OceanRenderer.Instance.LodDataResolution;
-            _shadowData = new RenderTexture(res, res, 0);
-            _shadowData.name = gameObject.name + "_oceanDepth";
-            _shadowData.format = TextureFormat;
-            _shadowData.useMipMap = false;
-            _shadowData.anisoLevel = 0;
+            for(int i = 0; i < 2; i++)
+            {
+                _shadowData[i] = new RenderTexture(res, res, 0);
+                _shadowData[i].name = gameObject.name + "_shadow_" + i;
+                _shadowData[i].format = TextureFormat;
+                _shadowData[i].useMipMap = false;
+                _shadowData[i].anisoLevel = 0;
+            }
 
             var lightGO = GameObject.Find("Directional light");
             if (lightGO)
@@ -37,8 +41,10 @@ namespace Crest
             _renderMaterial = new Material(Shader.Find("Ocean/ShadowUpdate"));
         }
 
-        private void Update()
+        protected override void LateUpdate()
         {
+            base.LateUpdate();
+
             if (!_mainLight) return;
 
             if (_bufCopyShadowMap == null)
@@ -48,13 +54,29 @@ namespace Crest
                 _mainLight.AddCommandBuffer(LightEvent.BeforeScreenspaceMask, _bufCopyShadowMap);
             }
 
+            // clear the shadow collection. it will be overwritten with shadow values IF the shadows render,
+            // which only happens if there are (nontransparent) shadow receivers around
+            Graphics.Blit(Texture2D.blackTexture, _shadowData[_rtIndex]);
+
             _bufCopyShadowMap.Clear();
-            _bufCopyShadowMap.SetRenderTarget(_shadowData);
-            _renderMaterial.SetVector("_CenterPos", transform.position);
+            _bufCopyShadowMap.SetRenderTarget(_shadowData[_rtIndex]);
+            LodTransform._renderData.Validate(0, this);
+            _renderMaterial.SetVector("_CenterPos", LodTransform._renderData._posSnapped);
             _renderMaterial.SetVector("_Scale", transform.lossyScale);
             _renderMaterial.SetVector("_CamPos", OceanRenderer.Instance._viewpoint.position);
             _renderMaterial.SetVector("_CamForward", OceanRenderer.Instance._viewpoint.forward);
-            _bufCopyShadowMap.Blit(Texture2D.blackTexture, _shadowData, _renderMaterial);
+
+            // TODO transfer shadows up/down lod chain
+            bool paramsOnly = false; // true if prev data didnt exist
+            _pwMat._target = _renderMaterial;
+            var rd = LodTransform._renderDataPrevFrame.Validate(-1, this);
+            BindData(0, _pwMat, paramsOnly ? Texture2D.blackTexture : (_shadowData[(_rtIndex + 1) % 2] as Texture), true, ref rd);
+            _pwMat._target = null;
+
+
+            _bufCopyShadowMap.Blit(Texture2D.blackTexture, _shadowData[_rtIndex], _renderMaterial);
+
+            _rtIndex = (_rtIndex + 1) % 2;
         }
 
         void OnEnable()
--- src/unity/Assets/Crest/Shaders/Simulation/Resources/ShadowUpdate.shader
+++ src/unity/Assets/Crest/Shaders/Simulation/Resources/ShadowUpdate.shader
@@ -16,10 +16,11 @@
 
 			#pragma enable_d3d11_debug_symbols
 
-			// this turns on all the shadowy stuff
+			// this turns on all the shady stuff (literally - its all deprecated)
 			#define SHADOW_COLLECTOR_PASS
 
 			#include "UnityCG.cginc"
+			#include "../../../../Crest/Shaders/OceanLODData.cginc"
 
 			struct appdata
 			{
@@ -35,7 +36,6 @@
 			uniform float3 _Scale;
 			uniform float3 _CamPos;
 			uniform float3 _CamForward;
-			uniform float3 _OceanCenterPosWorld;
 
 			v2f vert (appdata v)
 			{
@@ -49,7 +49,7 @@
 				// world pos from [0,1] quad
 				float4 wpos = float4(float3(v.vertex.x - 0.5, 0.0, v.vertex.y - 0.5) * _Scale.xzy * 4. + _CenterPos, 1.);
 
-				// TODO sea level... or maybe wave height/disp??
+				// TODO maybe wave height/disp??
 				wpos.y = _OceanCenterPosWorld.y;
 
 				o._WorldPosViewZ.xyz = wpos.xyz;
@@ -63,15 +63,35 @@
 				return o;
 			}
 
-			float4 ComputeShadow(v2f i)
+			// sadface - override macro used within SHADOW_COLLECTOR_FRAGMENT
+			#undef COMPUTE_SHADOW_COLLECTOR_SHADOW
+			#define COMPUTE_SHADOW_COLLECTOR_SHADOW(i, weights, shadowFade) \
+				float4 coord = float4(i._ShadowCoord0 * weights[0] + i._ShadowCoord1 * weights[1] + i._ShadowCoord2 * weights[2] + i._ShadowCoord3 * weights[3], 1); \
+				SAMPLE_SHADOW_COLLECTOR_SHADOW(coord) \
+				float res; \
+				res = saturate(shadow);
+
+			float ComputeShadow(v2f i, out float o_shadowFade)
 			{
 				SHADOW_COLLECTOR_FRAGMENT(i);
+
+				o_shadowFade = shadowFade;
+
+				return res;
 			}
 
 			float frag (v2f i) : SV_Target
 			{
-				float result = ComputeShadow(i).x;
-				return 1. - result;
+				float2 uv_lastframe = LD_0_WorldToUV(i._WorldPosViewZ.xz);
+
+				half lastShadow = 0.;
+				SampleShadow(_LD_Sampler_Shadow_0, uv_lastframe, 1.0, lastShadow);
+
+				float shadowFade;
+				float result = ComputeShadow(i, shadowFade).x;
+
+				float amountOfThisFrame = .02;
+				return lerp(lastShadow, 1. - result, amountOfThisFrame * (1. - shadowFade));
 			}
 			ENDCG
 		}

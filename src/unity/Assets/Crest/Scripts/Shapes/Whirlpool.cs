6e147191356b711351d15a37e5cfc03dbfb54e4a
Tom Maenan Read Cutting <readcuttingt@gmail.com>
2018-09-27 21:33:57 +0100

    Fixup formatting in Whirlpool.cs

--- src/unity/Assets/Crest/Scripts/Shapes/Whirlpool.cs
+++ src/unity/Assets/Crest/Scripts/Shapes/Whirlpool.cs
@@ -2,67 +2,68 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Crest {
+namespace Crest
+{
+    public class Whirlpool : MonoBehaviour
+    {
+        [Range(0, 1000)] public float amplitude = 10f;
+        [Range(0, 1000)] public float radius = 20f;
+        [Range(0, 1000)] public float eyeRadius = 1f;
+        [Range(0, 1000)] public float maxSpeed = 70f;
 
-public class Whirlpool : MonoBehaviour {
+        private GameObject _flow;
+        private GameObject _displacement;
+        private Material _flowMaterial;
+        private Material _displacementMaterial;
 
-
-    [Range(0, 1000)] public float amplitude = 10f;
-    [Range(0, 1000)] public float radius = 20f;
-    [Range(0, 1000)] public float eyeRadius = 1f;
-    [Range(0, 1000)] public float maxSpeed = 70f;
-
-    private GameObject _flow;
-    private GameObject _displacement;
-    private Material _flowMaterial;
-    private Material _displacementMaterial;
-
-    private void UpdateMaterials() {
-        _flowMaterial.SetFloat("_EyeRadiusProportion", eyeRadius/radius);
-        _flowMaterial.SetFloat("_MaxSpeed", maxSpeed);
-
-        _displacementMaterial.SetFloat("_Radius", radius * 0.25f);
-        _displacementMaterial.SetFloat("_Amplitude", amplitude);
-    }
-
-    // Use this for initialization
-    void Start () {
-        _flow = GameObject.CreatePrimitive(PrimitiveType.Quad);
-        _flow.name = "Flow";
-        _flow.transform.parent = transform;
-        _flow.transform.position = new Vector3(0f, -100f, 0f);
-        _flow.transform.localEulerAngles = new Vector3(90f, 0f, 0f);
-        _flow.transform.localScale = new Vector3(radius * 2f, radius * 2f, 1f);
-        _flow.AddComponent<ApplyLayers>();
+        private void UpdateMaterials()
         {
-            ApplyLayers applyLayers = _flow.GetComponent<ApplyLayers>();
-            applyLayers._layerName = "LodDataFlow";
+            _flowMaterial.SetFloat("_EyeRadiusProportion", eyeRadius/radius);
+            _flowMaterial.SetFloat("_MaxSpeed", maxSpeed);
+
+            _displacementMaterial.SetFloat("_Radius", radius * 0.25f);
+            _displacementMaterial.SetFloat("_Amplitude", amplitude);
         }
-        _flowMaterial = new Material(Shader.Find("Ocean/Shape/Whirlpool Flow"));
-        _flow.GetComponent<Renderer>().material = _flowMaterial;
 
-
-        _displacement = GameObject.CreatePrimitive(PrimitiveType.Quad);
-        _displacement.name = "Displacement";
-        _displacement.transform.parent = transform;
-        _displacement.transform.position = new Vector3(0f, -100f, 0f);
-        _displacement.transform.localEulerAngles = new Vector3(90f, 0f, 0f);
-        _displacement.AddComponent<ApplyLayers>();
+        // Use this for initialization
+        void Start ()
         {
-            ApplyLayers applyLayers = _displacement.GetComponent<ApplyLayers>();
-            applyLayers._layerName = "LodDataAnimatedWaves";
+            _flow = GameObject.CreatePrimitive(PrimitiveType.Quad);
+            _flow.name = "Flow";
+            _flow.transform.parent = transform;
+            _flow.transform.position = new Vector3(0f, -100f, 0f);
+            _flow.transform.localEulerAngles = new Vector3(90f, 0f, 0f);
+            _flow.transform.localScale = new Vector3(radius * 2f, radius * 2f, 1f);
+            _flow.AddComponent<ApplyLayers>();
+            {
+                ApplyLayers applyLayers = _flow.GetComponent<ApplyLayers>();
+                applyLayers._layerName = "LodDataFlow";
+            }
+            _flowMaterial = new Material(Shader.Find("Ocean/Shape/Whirlpool Flow"));
+            _flow.GetComponent<Renderer>().material = _flowMaterial;
+
+
+            _displacement = GameObject.CreatePrimitive(PrimitiveType.Quad);
+            _displacement.name = "Displacement";
+            _displacement.transform.parent = transform;
+            _displacement.transform.position = new Vector3(0f, -100f, 0f);
+            _displacement.transform.localEulerAngles = new Vector3(90f, 0f, 0f);
+            _displacement.AddComponent<ApplyLayers>();
+            {
+                ApplyLayers applyLayers = _displacement.GetComponent<ApplyLayers>();
+                applyLayers._layerName = "LodDataAnimatedWaves";
+            }
+            _displacementMaterial = new Material(Shader.Find("Ocean/Shape/Whirlpool Displacement"));
+            _displacement.GetComponent<Renderer>().material = _displacementMaterial;
+
+            UpdateMaterials();
         }
-        _displacementMaterial = new Material(Shader.Find("Ocean/Shape/Whirlpool Displacement"));
-        _displacement.GetComponent<Renderer>().material = _displacementMaterial;
 
-        UpdateMaterials();
+        // Update is called once per frame
+        void Update ()
+        {
+            OceanRenderer.Instance.ReportMaxDisplacementFromShape(0, amplitude);
+            UpdateMaterials();
+        }
     }
-
-    // Update is called once per frame
-    void Update () {
-        OceanRenderer.Instance.ReportMaxDisplacementFromShape(0, amplitude);
-        UpdateMaterials();
-    }
-}
-
 }

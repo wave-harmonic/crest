67a96e87dae736d6b5cfc6c066814e8408f3099a
Tom Maenan Read Cutting <tr395@cam.ac.uk>
2018-10-02 23:39:43 +0100

    Create a playpen scene to mess round with wave particles

--- src/unity/Assets/Crest/Scripts/LodData/LodDataWaveParticles.cs
+++ src/unity/Assets/Crest/Scripts/LodData/LodDataWaveParticles.cs
@@ -13,7 +13,7 @@ namespace Crest
         public Material convolutionMaterial;
 
         private WaveParticlesSystem _waveParticlesSystem = new WaveParticlesSystem();
-        private ExtendedHeightField _heightField = new ExtendedHeightField(8, 8, 100, 100);
+        private ExtendedHeightField _heightField = new ExtendedHeightField(8, 8, 1024, 1024);
         private int _frame = 0;
         private RenderTexture _finalTexture;
         private Texture2D _convolutionKernel;
@@ -73,16 +73,30 @@ namespace Crest
                 _convolutionKernel.SetPixels(kernelArray);
                 _convolutionKernel.Apply();
                 convolutionMaterial.SetTexture(Shader.PropertyToID("_KernelTex"), _convolutionKernel);
+                convolutionMaterial.SetFloat(Shader.PropertyToID("_Width"), _heightField.heightFieldInfo.Width);
+                convolutionMaterial.SetFloat(Shader.PropertyToID("_Height"), _heightField.heightFieldInfo.Height);
+                convolutionMaterial.SetInt(Shader.PropertyToID("_HoriRes"), _heightField.heightFieldInfo.HoriRes);
+                convolutionMaterial.SetInt(Shader.PropertyToID("_VertRes"), _heightField.heightFieldInfo.VertRes);
+                convolutionMaterial.SetFloat(Shader.PropertyToID("_ParticleRadii"), WaveParticle.RADIUS);
+                convolutionMaterial.SetFloat(Shader.PropertyToID("_KernelWidth"), kernelWidth);
+                convolutionMaterial.SetFloat(Shader.PropertyToID("_KernelHeight"), kernelHeight);
         }
 
+        bool doneFrame = false;
         void Update() {
+            if(doneFrame) return;
+            //doneFrame = true;
             RenderTexture.active = _heightField.textureHeightMap;
             GL.Clear(true, true, Color.black);
             RenderTexture.active = null;
 
             // Create a small ripple of particles, as it has a dispersion angle of 360
-            {
-                WaveParticle waveParticle = WaveParticle.createWaveParticle(Vector2.one * 4f, new Vector2(0.5f, 0.5f), 0.8f, Mathf.PI * 2, _frame);
+            if(_frame % 3 == 0) {
+                float x = Random.Range(0f, 2f);
+                if(x > 1) x = x + 7; else x = -x;
+                float y = Random.Range(0f, 2f);
+                if(y > 1) y = y + 7; else y = -y;
+                WaveParticle waveParticle = WaveParticle.createWaveParticle(new Vector2(x, y), new Vector2(0.5f, 0.5f), 10f, Mathf.PI * 2, _frame);
                 _waveParticlesSystem.addParticle(waveParticle);
             }
             _waveParticlesSystem.commitParticles();
@@ -92,8 +106,11 @@ namespace Crest
             RenderTexture.active = _finalTexture;
             GL.Clear(true, true, Color.black);
             Graphics.Blit(_heightField.textureHeightMap, _finalTexture, convolutionMaterial);
-
             _frame = (_frame + 1) % WaveParticle.FRAME_CYCLE_LENGTH;
+        }
+
+        void FixedUpdate() {
+            doneFrame = false;
         }
 
     }

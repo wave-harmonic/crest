b86d0a77f417bac817be80d70d770e6c738273c8
Huw Bowles <huw.bowles@gmail.com>
2018-11-04 16:16:29 +0000

    Lod data inputs - option to disable renderer

--- src/unity/Assets/Crest/Scripts/LodData/RegisterLodDataInput.cs
+++ src/unity/Assets/Crest/Scripts/LodData/RegisterLodDataInput.cs
@@ -5,9 +5,17 @@ namespace Crest
     public class RegisterLodDataInput<LodDataType> : MonoBehaviour
         where LodDataType : LodDataMgr
     {
+        [SerializeField] bool _disableRenderer = true;
+
         private void OnEnable()
         {
             var rend = GetComponent<Renderer>();
+
+            if (_disableRenderer)
+            {
+                rend.enabled = false;
+            }
+
             var ocean = OceanRenderer.Instance;
             if (rend && ocean)
             {

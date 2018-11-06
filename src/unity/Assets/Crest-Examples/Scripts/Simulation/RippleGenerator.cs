e1d96d2a7d3402f33c0e2f981bbff0f57d91fd50
Huw Bowles <huw.bowles@gmail.com>
2018-11-06 23:23:22 +0000

    Ripple test working

--- src/unity/Assets/Crest-Examples/Scripts/Simulation/RippleGenerator.cs
+++ src/unity/Assets/Crest-Examples/Scripts/Simulation/RippleGenerator.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using Crest;
+using UnityEngine;
 
 public class RippleGenerator : MonoBehaviour
 {
@@ -7,40 +8,37 @@ public class RippleGenerator : MonoBehaviour
     public float _onTime = 0.2f;
     public float _period = 4f;
 
-    MeshRenderer _mr;
     Material _mat;
+    RegisterDynWavesInput _rdwi;
 
 	void Start()
     {
-        if (Crest.OceanRenderer.Instance == null || !Crest.OceanRenderer.Instance._createDynamicWaveSim)
+        _rdwi = GetComponent<RegisterDynWavesInput>();
+
+        if (OceanRenderer.Instance == null || !OceanRenderer.Instance._createDynamicWaveSim || _rdwi == null)
         {
             enabled = false;
             return;
         }
 
-        _mr = GetComponent<MeshRenderer>();
-        if(_animate)
-        {
-            _mr.enabled = false;
-        }
-        _mat = _mr.material;
+        _mat = GetComponent<MeshRenderer>().material;
 	}
 	
 	void Update()
     {
         if(_animate)
         {
-            float t = Crest.OceanRenderer.Instance.CurrentTime;
+            float t = OceanRenderer.Instance.CurrentTime;
             if (t < _warmUp)
                 return;
             t -= _warmUp;
             t = Mathf.Repeat(t, _period);
-            _mr.enabled = t < _onTime;
+            _rdwi.enabled = t < _onTime;
         }
 
         // which lod is this object in (roughly)?
         Rect thisRect = new Rect(new Vector2(transform.position.x, transform.position.z), Vector3.zero);
-        int minLod = Crest.LodDataMgrAnimWaves.SuggestDataLOD(thisRect);
+        int minLod = LodDataMgrAnimWaves.SuggestDataLOD(thisRect);
         if (minLod == -1)
         {
             // outside all lods, nothing to update!
@@ -50,7 +48,7 @@ public class RippleGenerator : MonoBehaviour
         // how many active wave sims currently apply to this object - ideally this would eliminate sims that are too
         // low res, by providing a max grid size param
         int simsPresent, simsActive;
-        Crest.LodDataMgrDynWaves.CountWaveSims(minLod, out simsPresent, out simsActive);
+        LodDataMgrDynWaves.CountWaveSims(minLod, out simsPresent, out simsActive);
         if (simsPresent == 0)
         {
             enabled = false;
@@ -62,6 +60,6 @@ public class RippleGenerator : MonoBehaviour
             _mat.SetFloat("_SimCount", simsActive);
         }
 
-        _mat.SetFloat("_SimDeltaTime", Mathf.Min(Crest.LodDataMgrPersistent.MAX_SIM_DELTA_TIME, Time.deltaTime));
+        _mat.SetFloat("_SimDeltaTime", Mathf.Min(LodDataMgrPersistent.MAX_SIM_DELTA_TIME, Time.deltaTime));
     }
 }

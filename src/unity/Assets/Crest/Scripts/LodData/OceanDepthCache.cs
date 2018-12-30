2cad203ccc5bb2c473f1b44eadde91b54aac4507
Huw Bowles <huw.bowles@gmail.com>
2018-12-30 10:25:18 +0000

    Fixes #152
    Revert "Removed last of pointless auxillary cameras"
    
    This reverts commit ff07d3a4d689767e7d4c0a29b16a4fcf1670fa53.

--- src/unity/Assets/Crest/Scripts/LodData/OceanDepthCache.cs
+++ src/unity/Assets/Crest/Scripts/LodData/OceanDepthCache.cs
@@ -1,7 +1,7 @@
 ﻿// This file is subject to the MIT License as seen in the root of this folder structure (LICENSE)
 
 using UnityEngine;
-using UnityEngine.Rendering;
+using UnityEngine.Serialization;
 
 namespace Crest
 {
@@ -11,27 +11,25 @@ namespace Crest
     /// </summary>
     public class OceanDepthCache : MonoBehaviour
     {
-        [Tooltip("Can be disabled to delay population of the cache."), SerializeField]
-        bool _populateOnStartup = true;
+        [Tooltip("Can be disabled to delay population of the cache.")]
+        public bool _populateOnStartup = true;
 
-        [Tooltip("Renderers in scene to render into this depth cache. When provided this saves the code from doing an expensive FindObjectsOfType() call. If one or more renderers are specified, the layer setting is ignored."),  SerializeField]
-        Renderer[] _geometryToRenderIntoCache = new Renderer[0];
+        [Tooltip("The layers to render into the depth cache.")]
+        public string[] _layerNames;
 
-        [Tooltip("The layers to render into the depth cache. This is ignored if geometry instances are specified in the Geometry To Render Into Cache field."), SerializeField]
-        string[] _layerNames;
+        [Tooltip("The resolution of the cached depth - lower will be more efficient.")]
+        public int _resolution = 512;
 
-        [Tooltip("The resolution of the cached depth - lower will be more efficient."), SerializeField]
-        int _resolution = 512;
+        // a big hill will still want to write its height into the depth texture
+        [Tooltip("The 'near plane' for the depth cache camera (top down).")]
+        public float _cameraMaxTerrainHeight = 100f;
 
-        // A big hill will still want to write its height into the depth texture
-        [Tooltip("The 'near plane' for the depth cache camera (top down)."), SerializeField]
-        float _cameraMaxTerrainHeight = 100f;
+        [Tooltip("Will render into the cache every frame.")]
+        public bool _forceAlwaysUpdateDebug = false;
 
-        [Tooltip("Will render into the cache every frame. Intended for debugging, will generate garbage."), SerializeField]
-        bool _forceAlwaysUpdateDebug = false;
-
-        RenderTexture _cacheTexture;
+        RenderTexture _cache;
         GameObject _drawCacheQuad;
+        Camera _camDepthCache;
 
         void Start()
         {
@@ -64,79 +62,7 @@ namespace Crest
         }
 #endif
 
-        void InitObjects()
-        {
-            if (_cacheTexture == null)
-            {
-                _cacheTexture = new RenderTexture(_resolution, _resolution, 0);
-                _cacheTexture.name = gameObject.name + "_oceanDepth";
-                _cacheTexture.format = RenderTextureFormat.RHalf;
-                _cacheTexture.useMipMap = false;
-                _cacheTexture.anisoLevel = 0;
-            }
-
-            if (_drawCacheQuad == null)
-            {
-                _drawCacheQuad = GameObject.CreatePrimitive(PrimitiveType.Quad);
-                _drawCacheQuad.hideFlags = HideFlags.DontSave;
-                Destroy(_drawCacheQuad.GetComponent<Collider>());
-                _drawCacheQuad.name = "Draw_" + _cacheTexture.name + "_NOSAVE";
-                _drawCacheQuad.transform.SetParent(transform, false);
-                _drawCacheQuad.transform.localEulerAngles = 90f * Vector3.right;
-                _drawCacheQuad.AddComponent<RegisterSeaFloorDepthInput>();
-                var qr = _drawCacheQuad.GetComponent<Renderer>();
-                qr.material = new Material(Shader.Find("Ocean/Inputs/Depth/Cached Depths"));
-                qr.material.mainTexture = _cacheTexture;
-                qr.enabled = false;
-            }
-        }
-
         public void PopulateCache()
-        {
-            // Make sure we have required objectss
-            InitObjects();
-
-            // Render into the cache
-            {
-                var buf = new CommandBuffer();
-                buf.name = "Populate " + gameObject.name;
-
-                buf.SetRenderTarget(_cacheTexture);
-                buf.ClearRenderTarget(true, true, Color.black);
-
-                var worldToCameraMatrix =
-                    LodTransform.CalculateWorldToCameraMatrixRHS(transform.position + Vector3.up * _cameraMaxTerrainHeight, transform.rotation * Quaternion.AngleAxis(90f, Vector3.right));
-                var projectionMatrix =
-                    Matrix4x4.Ortho(-0.5f * transform.lossyScale.x, 0.5f * transform.lossyScale.x, -0.5f * transform.lossyScale.z, 0.5f * transform.lossyScale.z, 0.5f, 500f);
-                buf.SetViewProjectionMatrices(worldToCameraMatrix, projectionMatrix);
-
-                var mat = new Material(Shader.Find("Ocean/Inputs/Depth/Ocean Depth From Geometry"));
-                mat.SetVector("_OceanCenterPosWorld", OceanRenderer.Instance.transform.position);
-
-                if (_geometryToRenderIntoCache != null && _geometryToRenderIntoCache.Length > 0)
-                {
-                    PopulateUsingGeometryList(mat, buf);
-                }
-                else
-                {
-                    PopulateUsingLayers(mat, buf);
-                }
-
-                Graphics.ExecuteCommandBuffer(buf);
-            }
-        }
-
-        void PopulateUsingGeometryList(Material mat, CommandBuffer buf)
-        {
-            Debug.Assert(_geometryToRenderIntoCache != null && _geometryToRenderIntoCache.Length > 0);
-
-            foreach (var rend in _geometryToRenderIntoCache)
-            {
-                buf.DrawRenderer(rend, mat);
-            }
-        }
-
-        void PopulateUsingLayers(Material mat, CommandBuffer buf)
         {
             var layerMask = 0;
             foreach (var layer in _layerNames)
@@ -144,7 +70,7 @@ namespace Crest
                 int layerIdx = LayerMask.NameToLayer(layer);
                 if (string.IsNullOrEmpty(layer) || layerIdx == -1)
                 {
-                    Debug.LogError("OceanDepthCache: Invalid layer specified: \"" + layer +
+                    Debug.LogError("OceanDepthCache: Invalid layer specified: \"" + layer + 
                         "\". Please specify valid layers for objects/geometry that provide the ocean depth.", this);
                 }
                 else
@@ -152,29 +78,62 @@ namespace Crest
                     layerMask = layerMask | (1 << layerIdx);
                 }
             }
-
             if (layerMask == 0)
             {
                 Debug.LogError("No valid layers for populating depth cache, aborting.", this);
-                return;
+            }
+            
+            if (_cache == null)
+            {
+                _cache = new RenderTexture(_resolution, _resolution, 0);
+                _cache.name = gameObject.name + "_oceanDepth";
+                _cache.format = RenderTextureFormat.RHalf;
+                _cache.useMipMap = false;
+                _cache.anisoLevel = 0;
             }
 
-            var gos = FindObjectsOfType<GameObject>();
-            foreach (var go in gos)
+            if (_drawCacheQuad == null)
             {
-                if (0 == ((1 << go.layer) & layerMask)) continue;
-                var rend = go.GetComponent<Renderer>();
-                if (rend == null) continue;
-                buf.DrawRenderer(rend, mat);
+                _drawCacheQuad = GameObject.CreatePrimitive(PrimitiveType.Quad);
+                Destroy(_drawCacheQuad.GetComponent<Collider>());
+                _drawCacheQuad.name = "Draw_" + _cache.name;
+                _drawCacheQuad.transform.SetParent(transform, false);
+                _drawCacheQuad.transform.localEulerAngles = 90f * Vector3.right;
+                _drawCacheQuad.AddComponent<RegisterSeaFloorDepthInput>();
+                var qr = _drawCacheQuad.GetComponent<Renderer>();
+                qr.material = new Material(Shader.Find("Ocean/Inputs/Depth/Cached Depths"));
+                qr.material.mainTexture = _cache;
+                qr.enabled = false;
             }
+
+            if (_camDepthCache == null)
+            {
+                _camDepthCache = new GameObject("DepthCacheCam").AddComponent<Camera>();
+                _camDepthCache.transform.position = transform.position + Vector3.up * _cameraMaxTerrainHeight;
+                _camDepthCache.transform.parent = transform;
+                _camDepthCache.transform.localEulerAngles = 90f * Vector3.right;
+                _camDepthCache.orthographic = true;
+                _camDepthCache.orthographicSize = Mathf.Max(transform.lossyScale.x / 2f, transform.lossyScale.z / 2f);
+                _camDepthCache.targetTexture = _cache;
+                _camDepthCache.cullingMask = layerMask;
+                _camDepthCache.clearFlags = CameraClearFlags.SolidColor;
+                // 0 means '0m above very deep sea floor'
+                _camDepthCache.backgroundColor = Color.black;
+                _camDepthCache.enabled = false;
+                _camDepthCache.allowMSAA = false;
+                // I'd prefer to destroy the cam object, but I found sometimes (on first start of editor) it will fail to render.
+                _camDepthCache.gameObject.SetActive(false);
+            }
+
+            // Hackety-hack: this seems to be the only way to pass parameters to the shader when using RenderWithShader!
+            Shader.SetGlobalVector("_OceanCenterPosWorld", OceanRenderer.Instance.transform.position);
+            _camDepthCache.RenderWithShader(Shader.Find("Ocean/Inputs/Depth/Ocean Depth From Geometry"), null);
         }
 
-#if UNITY_EDITOR
         void OnDrawGizmosSelected()
         {
             Gizmos.matrix = transform.localToWorldMatrix;
             Gizmos.DrawWireCube(Vector3.zero, new Vector3(1f, 0f, 1f));
         }
-#endif
     }
 }

31c12b6e042c8fae13117a35602df6cf9f168419
Tom Maenan Read Cutting <readcuttingt@gmail.com>
2019-06-02 22:13:53 +0000

    Remove implicit shader truncation warnings

--- crest/Assets/Crest/Crest/Shaders/OceanLODDataShared.hlsl
+++ crest/Assets/Crest/Crest/Shaders/OceanLODDataShared.hlsl
@@ -117,7 +117,7 @@ float2 IDtoUV(in float2 i_id)
 // Sampling functions
 void SampleDisplacements(in Texture2DArray i_dispSampler, in float3 i_uv_slice, in float i_wt, inout float3 io_worldPos)
 {
-	const half3 disp = i_dispSampler.SampleLevel(LODData_linear_clamp_sampler, i_uv_slice, float2(0, 0)).xyz;
+	const half3 disp = i_dispSampler.SampleLevel(LODData_linear_clamp_sampler, i_uv_slice, 0).xyz;
 	io_worldPos += i_wt * disp;
 }
 
@@ -128,8 +128,8 @@ void SampleDisplacementsNormals(in Texture2DArray i_dispSampler, in float3 i_uv_
 
 	float3 n; {
 		float3 dd = float3(i_invRes, 0.0, i_texelSize);
-		half3 disp_x = dd.zyy + i_dispSampler.SampleLevel(LODData_linear_clamp_sampler, i_uv_slice + float3(dd.xy, 0), dd.yy).xyz;
-		half3 disp_z = dd.yyz + i_dispSampler.SampleLevel(LODData_linear_clamp_sampler, i_uv_slice + float3(dd.yx, 0), dd.yy).xyz;
+		half3 disp_x = dd.zyy + i_dispSampler.SampleLevel(LODData_linear_clamp_sampler, i_uv_slice + float3(dd.xy, 0), dd.y).xyz;
+		half3 disp_z = dd.yyz + i_dispSampler.SampleLevel(LODData_linear_clamp_sampler, i_uv_slice + float3(dd.yx, 0), dd.y).xyz;
 		n = normalize(cross(disp_z - disp, disp_x - disp));
 	}
 	io_nxz += i_wt * n.xz;
@@ -137,17 +137,17 @@ void SampleDisplacementsNormals(in Texture2DArray i_dispSampler, in float3 i_uv_
 
 void SampleFoam(in Texture2DArray i_oceanFoamSampler, in float3 i_uv_slice, in float i_wt, inout half io_foam)
 {
-	io_foam += i_wt * i_oceanFoamSampler.SampleLevel(LODData_linear_clamp_sampler, i_uv_slice, float2(0, 0)).x;
+	io_foam += i_wt * i_oceanFoamSampler.SampleLevel(LODData_linear_clamp_sampler, i_uv_slice, 0).x;
 }
 
 void SampleFlow(in Texture2DArray i_oceanFlowSampler, in float3 i_uv_slice, in float i_wt, inout half2 io_flow)
 {
-	io_flow += i_wt * i_oceanFlowSampler.SampleLevel(LODData_linear_clamp_sampler, i_uv_slice, float2(0, 0)).xy;
+	io_flow += i_wt * i_oceanFlowSampler.SampleLevel(LODData_linear_clamp_sampler, i_uv_slice, 0).xy;
 }
 
 void SampleSeaDepth(in Texture2DArray i_oceanDepthSampler, in float3 i_uv_slice, in float i_wt, inout half io_oceanDepth)
 {
-	io_oceanDepth += i_wt * (i_oceanDepthSampler.SampleLevel(LODData_linear_clamp_sampler, i_uv_slice, float2(0.0, 0.0)).x - CREST_OCEAN_DEPTH_BASELINE);
+	io_oceanDepth += i_wt * (i_oceanDepthSampler.SampleLevel(LODData_linear_clamp_sampler, i_uv_slice, 0).x - CREST_OCEAN_DEPTH_BASELINE);
 }
 
 void SampleShadow(in Texture2DArray i_oceanShadowSampler, in float3 i_uv_slice, in float i_wt, inout half2 io_shadow)
@@ -155,19 +155,10 @@ void SampleShadow(in Texture2DArray i_oceanShadowSampler, in float3 i_uv_slice, 
 	io_shadow += i_wt * i_oceanShadowSampler.Sample(LODData_linear_clamp_sampler, i_uv_slice).xy;
 }
 
-
-#define SampleLod(i_lodTextureArray, i_uv_slice) (i_lodTextureArray.SampleLevel(LODData_linear_clamp_sampler, i_uv_slice, float2(0, 0)))
+// TODO(MRT): replace these with something better in code that uses them.
+// Used #defines to account for different return types based on texture format.
+#define SampleLod(i_lodTextureArray, i_uv_slice) (i_lodTextureArray.SampleLevel(LODData_linear_clamp_sampler, i_uv_slice, 0))
 #define SampleLodLevel(i_lodTextureArray, i_uv_slice, mips) (i_lodTextureArray.SampleLevel(LODData_linear_clamp_sampler, i_uv_slice, mips))
-
-// void SampleLod(in Texture2DArray i_lodTextureArray, in float3 i_uv_slice)
-// {
-// 	return i_lodTextureArray.SampleLevel(LODData_linear_clamp_sampler, i_uv_slice, float2(0, 0));
-// }
-
-// void SampleLod(in Texture2DArray i_lodTextureArray, in float3 i_uv_slice, in float2 mips)
-// {
-// 	return i_lodTextureArray.SampleLevel(LODData_linear_clamp_sampler, i_uv_slice, mips);
-// }
 
 // Geometry data
 // x: Grid size of lod data - size of lod data texel in world space.

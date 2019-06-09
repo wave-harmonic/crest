092a46dfa7700f9b7e3df943285261909189a70e
Huw Bowles <huw.bowles@gmail.com>
2019-06-09 10:38:20 +0100

    Add more generic floating object scripts

---
+++ crest/Assets/Crest/Crest/Scripts/Interaction/FloatingObjectBase.cs
@@ -0,0 +1,17 @@
+﻿// This file is subject to the MIT License as seen in the root of this folder structure (LICENSE)
+
+using UnityEngine;
+
+namespace Crest
+{
+    /// <summary>
+    /// Base class for objects that float on water.
+    /// </summary>
+    public abstract class FloatingObjectBase : MonoBehaviour
+    {
+        public abstract Vector3 DisplacementToObject { get; set; }
+        public abstract float ObjectWidth { get; }
+        public abstract bool InWater { get; }
+        public abstract Rigidbody RB { get; set; }
+    }
+}

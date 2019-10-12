e00f6a2d9cb370c06239889d204ff7b43f5dee00
Huw Bowles <huw.bowles@gmail.com>
2019-10-12 17:23:31 +0100

    Make compute shader queries the default

--- crest/Assets/Crest/Crest/Scripts/LodData/Settings/SimSettingsAnimatedWaves.cs
+++ crest/Assets/Crest/Crest/Scripts/LodData/Settings/SimSettingsAnimatedWaves.cs
@@ -22,11 +22,11 @@ namespace Crest
         }
         [Header("Readback to CPU")]
         [Tooltip("Where to obtain ocean shape on CPU for physics / gameplay."), SerializeField]
-        CollisionSources _collisionSource = CollisionSources.OceanDisplacementTexturesGPU;
+        CollisionSources _collisionSource = CollisionSources.ComputeShaderQueries;
         public CollisionSources CollisionSource { get { return _collisionSource; } }
 
         [SerializeField, Tooltip("Cache CPU requests for ocean height. Requires restart.")]
-        bool _cachedHeightQueries = true;
+        bool _cachedHeightQueries = false;
         public bool CachedHeightQueries { get { return _cachedHeightQueries; } }
 
         [Header("GPU Readback Settings")]

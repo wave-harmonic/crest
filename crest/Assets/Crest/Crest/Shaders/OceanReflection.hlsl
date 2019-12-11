388451523a6abc32c440c6d7fd39c5453d3352ef
HelloKitty <andrew.blakely@ymail.com>
2019-12-10 21:58:12 -0600

    Added Unity PBS Reflection Probe Sampling
    This probe sampling code is based on the Unity PBS standard shader and interpolation code suggested by https://catlikecoding.com/unity/tutorials/rendering/part-8/.
    
    It should blend probes, which I think was not taking place before, and also it should make reflection smoothness depend on the Ocean specular parameter.

--- crest/Assets/Crest/Crest/Shaders/OceanReflection.hlsl
+++ crest/Assets/Crest/Crest/Shaders/OceanReflection.hlsl
@@ -82,9 +82,26 @@ void ApplyReflectionSky(in const half3 i_view, in const half3 i_n_pixel, in cons
 	half4 val = texCUBE(_ReflectionCubemapOverride, refl);
 	skyColour = val.rgb;
 #else
-	// Unity specular reflection cubemap
-	half4 val = UNITY_SAMPLE_TEXCUBE_LOD(unity_SpecCube0, refl, 0.);
-	skyColour = DecodeHDR(val, unity_SpecCube0_HDR);
+	Unity_GlossyEnvironmentData envData;
+	envData.roughness = 1.0f - _Specular;
+	envData.reflUVW = refl;
+	float3 probe0 = Unity_GlossyEnvironment(UNITY_PASS_TEXCUBE(unity_SpecCube0), unity_SpecCube0_HDR, envData);
+	#if UNITY_SPECCUBE_BLENDING
+		float interpolator = unity_SpecCube0_BoxMin.w;
+		// Branch optimization recommended by: https://catlikecoding.com/unity/tutorials/rendering/part-8/
+		UNITY_BRANCH
+		if (interpolator < 0.99999) 
+		{
+			float3 probe1 = Unity_GlossyEnvironment(UNITY_PASS_TEXCUBE_SAMPLER(unity_SpecCube1, unity_SpecCube0), unity_SpecCube0_HDR, envData);
+			skyColour = lerp(probe1, probe0, interpolator);
+		}
+		else 
+		{
+			skyColour = probe0;
+		}
+	#else
+		skyColour = probe0;
+	#endif
 #endif
 
 #endif

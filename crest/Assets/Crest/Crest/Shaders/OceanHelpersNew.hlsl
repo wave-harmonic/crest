dabfb2226421305ba2a689c1379baf9a03bc568a
Dale Eidd <dale@eidd.name>
2020-03-02 22:32:32 +1100

    Fix license

--- crest/Assets/Crest/Crest/Shaders/OceanHelpersNew.hlsl
+++ crest/Assets/Crest/Crest/Shaders/OceanHelpersNew.hlsl
@@ -1,6 +1,6 @@
 // Crest Ocean System
 
-// Copyright 2020 Wave Harmonic Ltd
+// This file is subject to the MIT License as seen in the root of this folder structure (LICENSE)
 
 // Ocean LOD data - data, samplers and functions associated with LODs
 

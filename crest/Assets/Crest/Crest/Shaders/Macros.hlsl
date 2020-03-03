fcca94a5a06507e86657e795b9969cd68cb2c854
Dale Eidd <dale@eidd.name>
2020-03-04 02:13:37 +1100

    Fix #ifndef guard typo

--- crest/Assets/Crest/Crest/Shaders/Macros.hlsl
+++ crest/Assets/Crest/Crest/Shaders/Macros.hlsl
@@ -5,11 +5,11 @@
 // MACROS - Macros to help with cross pipeline development
 
 #ifndef CREST_MACROS_H
-#define CREST_MACROS
+#define CREST_MACROS_H
 
 #ifndef CBUFFER_START
 #define CBUFFER_START(name)
 #define CBUFFER_END
 #endif
 
-#endif // CREST_MACROS
+#endif // CREST_MACROS_H

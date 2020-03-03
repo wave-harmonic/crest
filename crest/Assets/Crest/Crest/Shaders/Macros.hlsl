b7d529a79988e57407a127206a95cb02662e3bea
Dale Eidd <dale@eidd.name>
2020-03-04 02:41:03 +1100

    Implement cbuffer macro stub
    Adapted from PPv2 stack

--- crest/Assets/Crest/Crest/Shaders/Macros.hlsl
+++ crest/Assets/Crest/Crest/Shaders/Macros.hlsl
@@ -7,9 +7,18 @@
 #ifndef CREST_MACROS_H
 #define CREST_MACROS_H
 
+// Adapted from PPv2 stack. These macros aren't defined for compute shaders.
 #ifndef CBUFFER_START
-#define CBUFFER_START(name)
-#define CBUFFER_END
+#if defined(SHADER_API_VULKAN) || defined(SHADER_API_D3D11) || defined(SHADER_API_D3D12) || defined(SHADER_API_METAL) || defined(SHADER_API_SWITCH) || defined(SHADER_API_XBOXONE)
+    #define CBUFFER_START(name) cbuffer name {
+    #define CBUFFER_END };
+#elif defined(SHADER_API_PSSL)
+    #define CBUFFER_START(name) ConstantBuffer name {
+    #define CBUFFER_END };
+#else
+    #define CBUFFER_START(name)
+    #define CBUFFER_END
+#endif
 #endif
 
 #endif // CREST_MACROS_H

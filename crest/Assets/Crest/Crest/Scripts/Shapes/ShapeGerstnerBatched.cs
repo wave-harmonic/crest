fae3eaa2563024c0f4379b0474a6a7b50cbbb380
Tom Maenan Read Cutting <readcuttingt@gmail.com>
2020-03-11 18:53:32 +0000

    Remove errant garbage generation that was happening each frame

--- crest/Assets/Crest/Crest/Scripts/Shapes/ShapeGerstnerBatched.cs
+++ crest/Assets/Crest/Crest/Scripts/Shapes/ShapeGerstnerBatched.cs
@@ -236,7 +236,10 @@ namespace Crest
 
         private void ReportMaxDisplacement()
         {
-            Debug.Assert(_spectrum._chopScales.Length == OceanWaveSpectrum.NUM_OCTAVES, $"OceanWaveSpectrum {_spectrum.name} is out of date, please open this asset and resave in editor.", _spectrum);
+            if(_spectrum._chopScales.Length != OceanWaveSpectrum.NUM_OCTAVES)
+            {
+                Debug.LogError($"OceanWaveSpectrum {_spectrum.name} is out of date, please open this asset and resave in editor.", _spectrum);
+            }
 
             float ampSum = 0f;
             for (int i = 0; i < _wavelengths.Length; i++)

1282df473b908574251b54f7d43c8a7e2d2dc6f3
Dale Eidd <dale@eidd.name>
2020-03-13 12:35:15 +1100

    Add "Convex Hull Options" header

--- crest/Assets/Crest/Crest/Scripts/LodData/RegisterClipSurfaceInput.cs
+++ crest/Assets/Crest/Crest/Scripts/LodData/RegisterClipSurfaceInput.cs
@@ -25,10 +25,12 @@ namespace Crest
         [Tooltip("Use 'None' if you want to use your own material.")]
         public ClipSurfaceShaders runtimeAssignMaterial;
 
+        [Header("Convex Hull Options")]
+
         [Tooltip("Prevents inputs from cancelling each other out when aligned vertically. It is imperfect so custom logic might be needed for your use case.")]
         [SerializeField] bool _disableClipSurfaceWhenTooFarFromSurface;
 
-        [Tooltip("Large, choppy waves require higher iterations to have accurate holes. Only used with convex hulls.")]
+        [Tooltip("Large, choppy waves require higher iterations to have accurate holes.")]
         [SerializeField] uint _animatedWavesDisplacementSamplingIterations = 4;
 
         public override float Wavelength => 0f;

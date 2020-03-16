e717f19b9cfd2d4c896b78a7c81c90a5aad1af29
Huw Bowles <huw.bowles@gmail.com>
2020-03-16 14:04:10 +0000

    Knock it all out - waves wont appear but does it hitch?

--- crest/Assets/Crest/Crest/Shaders/OceanInputs/GerstnerShared.hlsl
+++ crest/Assets/Crest/Crest/Shaders/OceanInputs/GerstnerShared.hlsl
@@ -25,54 +25,56 @@ CBUFFER_END
 
 half4 ComputeGerstner(float2 worldPosXZ, float3 uv_slice)
 {
-	float2 displacementNormalized = 0.0;
+	return (half4)0.25;
 
-	// sample ocean depth (this render target should 1:1 match depth texture, so UVs are trivial)
-	const half depth = 100.0; // _LD_TexArray_SeaFloorDepth.Sample(LODData_linear_clamp_sampler, uv_slice).x;
+	//float2 displacementNormalized = 0.0;
 
-	half3 result = (half3)0.0;
+	//// sample ocean depth (this render target should 1:1 match depth texture, so UVs are trivial)
+	//const half depth = 100.0; // _LD_TexArray_SeaFloorDepth.Sample(LODData_linear_clamp_sampler, uv_slice).x;
 
-	// attenuate waves based on ocean depth. if depth is greater than 0.5*wavelength, water is considered Deep and wave is
-	// unaffected. if depth is less than this, wave velocity decreases. waves will then bunch up and grow in amplitude and
-	// eventually break. i model "Deep" water, but then simply ramp down waves in non-deep water with a linear multiplier.
-	// http://hyperphysics.phy-astr.gsu.edu/hbase/Waves/watwav2.html
-	// http://hyperphysics.phy-astr.gsu.edu/hbase/watwav.html#c1
-	// optimisation - do this outside the loop below - take the median wavelength for depth weighting, intead of computing
-	// per component. computing per component makes little difference to the end result
-	half depth_wt = saturate(depth * _TwoPiOverWavelengths[_NumWaveVecs / 2].x / PI);
-	half4 wt = 1.0; // _AttenuationInShallows * depth_wt + (1.0 - _AttenuationInShallows);
+	//half3 result = (half3)0.0;
 
-	// gerstner computation is vectorized - processes 4 wave components at once
-	for (uint vi = 0; vi < 1; vi++)
-	{
-		// direction
-		half4 Dx = 1.0; // _WaveDirX[vi];
-		half4 Dz = 0.0; // _WaveDirZ[vi];
+	//// attenuate waves based on ocean depth. if depth is greater than 0.5*wavelength, water is considered Deep and wave is
+	//// unaffected. if depth is less than this, wave velocity decreases. waves will then bunch up and grow in amplitude and
+	//// eventually break. i model "Deep" water, but then simply ramp down waves in non-deep water with a linear multiplier.
+	//// http://hyperphysics.phy-astr.gsu.edu/hbase/Waves/watwav2.html
+	//// http://hyperphysics.phy-astr.gsu.edu/hbase/watwav.html#c1
+	//// optimisation - do this outside the loop below - take the median wavelength for depth weighting, intead of computing
+	//// per component. computing per component makes little difference to the end result
+	//half depth_wt = saturate(depth * _TwoPiOverWavelengths[_NumWaveVecs / 2].x / PI);
+	//half4 wt = 1.0; // _AttenuationInShallows * depth_wt + (1.0 - _AttenuationInShallows);
 
-		// wave number
-		half4 k = 0.5; // _TwoPiOverWavelengths[vi];
-		// spatial location
-		half4 x = Dx * worldPosXZ.x + Dz * worldPosXZ.y;
-		half4 angle = k * x + vi; // +_Phases[vi];
+	//// gerstner computation is vectorized - processes 4 wave components at once
+	//for (uint vi = 0; vi < 1; vi++)
+	//{
+	//	// direction
+	//	half4 Dx = 1.0; // _WaveDirX[vi];
+	//	half4 Dz = 0.0; // _WaveDirZ[vi];
 
-		// dx and dz could be baked into _ChopAmps
-		half4 disp = 0.125 * sin(angle);
-		half4 resultx = disp * Dx;
-		half4 resultz = disp * Dz;
+	//	// wave number
+	//	half4 k = 0.5; // _TwoPiOverWavelengths[vi];
+	//	// spatial location
+	//	half4 x = Dx * worldPosXZ.x + Dz * worldPosXZ.y;
+	//	half4 angle = k * x + vi; // +_Phases[vi];
 
-		half4 resulty = 0.125 * cos(angle);
+	//	// dx and dz could be baked into _ChopAmps
+	//	half4 disp = 0.125 * sin(angle);
+	//	half4 resultx = disp * Dx;
+	//	half4 resultz = disp * Dz;
 
-		// sum the vector results
-		result.x += dot(resultx, wt);
-		result.y += dot(resulty, wt);
-		result.z += dot(resultz, wt);
+	//	half4 resulty = 0.125 * cos(angle);
 
-		//half4 sssFactor = 1.0; // min(1.0, _TwoPiOverWavelengths[vi]);
-		//displacementNormalized.x += dot(resultx * sssFactor, wt);
-		//displacementNormalized.y += dot(resultz * sssFactor, wt);
-	}
+	//	// sum the vector results
+	//	result.x += dot(resultx, wt);
+	//	result.y += dot(resulty, wt);
+	//	result.z += dot(resultz, wt);
+
+	//	//half4 sssFactor = 1.0; // min(1.0, _TwoPiOverWavelengths[vi]);
+	//	//displacementNormalized.x += dot(resultx * sssFactor, wt);
+	//	//displacementNormalized.y += dot(resultz * sssFactor, wt);
+	//}
 
 	//half sss = length(displacementNormalized);
 
-	return _Weight * half4(result, 0.1);
+	//return _Weight * half4(result, 0.1);
 }

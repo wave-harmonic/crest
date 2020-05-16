6279f4026bc1d9de011c609c03f80e6d47cceb80
Dale Eidd <dale@eidd.name>
2020-05-16 21:22:53 +1000

    Add resolution debugger

---
+++ crest/Assets/Development/Scripts/ResolutionDebugger.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[ExecuteAlways]
+public class ResolutionDebugger : MonoBehaviour
+{
+    [SerializeField] int _screenWidth = 0;
+    [SerializeField] int _screenHeight = 0;
+
+    void Update()
+    {
+        _screenWidth = Screen.width;
+        _screenHeight = Screen.height;
+    }
+}

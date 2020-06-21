82b1ed3971d31aba51a26cbbac2f9fe90b65888a
Huw Bowles <huw.bowles@gmail.com>
2020-06-21 12:51:14 +0100

    Take into account the emitter velocity

--- crest/Assets/Crest/Crest/Scripts/FX/EmitParticlesWhenSubmerged.cs
+++ crest/Assets/Crest/Crest/Scripts/FX/EmitParticlesWhenSubmerged.cs
@@ -23,12 +23,19 @@ namespace Crest
         [Tooltip("Scale value for particle speed multiplier. Time axis is proportion of the Minimum Velocity setting. Value of curve at Time=1 is used if water vel exactly matches minimum emission velocity. Value of curve at Time=3 is used if water vel is 3x greater than the minimum velocity."), SerializeField]
         AnimationCurve _initalVelVsWaterVel2 = new AnimationCurve(new Keyframe[] { new Keyframe(1f, 1f), new Keyframe(4f, 5f) });
 
+        [Tooltip("If false, script will wait until particle system is not playing before emitting again."), SerializeField]
+        bool _allowMultipleSimultaneousEmissions = false;
+
         [Header("Debug Settings")]
         [Tooltip(""), SerializeField]
         bool _logEvents = false;
 
         ParticleSystem _particleSystem = null;
         SampleHeightHelper _sampleHeightHelper = new SampleHeightHelper();
+
+        Vector3 _lastPos = Vector3.zero;
+        bool _lastPosValid = false;
+        Vector3 _thisVelocity = Vector3.zero;
 
         private void Awake()
         {
@@ -43,17 +50,31 @@ namespace Crest
 
         void Update()
         {
-            if (_particleSystem.isEmitting)
-                return;
+            if (_lastPosValid && Time.deltaTime > 0.0001f)
+            {
+                _thisVelocity = (transform.position - _lastPos) / Time.deltaTime;
+            }
+            _lastPos = transform.position;
+            _lastPosValid = true;
 
             _sampleHeightHelper.Init(transform.position, _minWavelength * 2f);
 
             float height = 0f;
             var dummy = Vector3.zero;
             var vel = Vector3.zero;
-            if (_sampleHeightHelper.Sample(ref height, ref dummy, ref vel))
+            var sampled = _sampleHeightHelper.Sample(ref height, ref dummy, ref vel);
+
+            if (_particleSystem.isEmitting || (_particleSystem.isPlaying && !_allowMultipleSimultaneousEmissions))
             {
-                if (Mathf.Abs(height - transform.position.y) < _maximumHeightDifference && vel.y > _minimumVelocity && !_particleSystem.isPlaying)
+                return;
+            }
+
+            if (sampled)
+            {
+                // Compensate for motion of this emitter object
+                vel -= _thisVelocity;
+
+                if (Mathf.Abs(height - transform.position.y) < _maximumHeightDifference && vel.y > _minimumVelocity)
                 {
                     _particleSystem.Play();
 

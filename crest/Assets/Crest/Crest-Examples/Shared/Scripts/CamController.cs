28f291790f2cc51511b0aa4ec9dff2773e2b5b3e
Dale Eidd <dale@eidd.name>
2020-07-24 12:06:42 +0800

    Add disable camera kill roll debug option

--- crest/Assets/Crest/Crest-Examples/Shared/Scripts/CamController.cs
+++ crest/Assets/Crest/Crest-Examples/Shared/Scripts/CamController.cs
@@ -23,6 +23,9 @@ public class CamController : MonoBehaviour
     [System.Serializable]
     class DebugFields
     {
+        [Tooltip("Disables controller preventing the camera from rolling (rotating on the z axis).")]
+        public bool disableCameraRollPrevention = false;
+
         [Tooltip("Disables the XR occlusion mesh for debugging purposes. Only works with legacy XR.")]
         public bool disableOcclusionMesh = false;
 
@@ -150,6 +153,7 @@ public class CamController : MonoBehaviour
 
     void UpdateKillRoll()
     {
+        if (_debug.disableCameraRollPrevention) return;
         Vector3 ea = _targetTransform.eulerAngles;
         ea.z = 0f;
         transform.eulerAngles = ea;

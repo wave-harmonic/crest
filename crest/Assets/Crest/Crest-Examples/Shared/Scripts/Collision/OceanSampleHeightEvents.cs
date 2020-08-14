a6b325cd1b0b237494d8f6f18be4595f163070b5
Dale Eidd <dale@eidd.name>
2020-08-14 12:24:45 -0700

    Add "Distance From Ocean Surface" event

--- crest/Assets/Crest/Crest-Examples/Shared/Scripts/Collision/OceanSampleHeightEvents.cs
+++ crest/Assets/Crest/Crest-Examples/Shared/Scripts/Collision/OceanSampleHeightEvents.cs
@@ -17,15 +17,36 @@ public class OceanSampleHeightEvents : MonoBehaviour
 {
     [Tooltip("The higher the value, the more smaller waves will be ignored.")]
     [SerializeField] float _samplingLengthScale = 1f;
+
+
+    [Header("Distance From Ocean Surface")]
+
+    [Tooltip("Distance from ocean surface will be between zero and one.")]
+    [SerializeField] bool _normaliseDistance = true;
+
+    [Tooltip("The maximum distance passed to function. Always use a real distance value (not a normalised one).")]
+    [SerializeField] float _maximumDistance = 100f;
+
+    [Tooltip("Apply a curve to the distance passed to the function.")]
+    [SerializeField] AnimationCurve _distanceCurve = AnimationCurve.Linear(0f, 0f, 1f, 1f);
+
+
+    [Header("Events")]
+
     [SerializeField] UnityEvent _onBelowOceanSurface = new UnityEvent();
     public UnityEvent OnBelowOceanSurface => _onBelowOceanSurface;
     [SerializeField] UnityEvent _onAboveOceanSurface = new UnityEvent();
     public UnityEvent OnAboveOceanSurface => _onAboveOceanSurface;
+    [SerializeField] FloatEvent _distanceFromOceanSurface = new FloatEvent();
+    public FloatEvent DistanceFromOceanSurface => _distanceFromOceanSurface;
 
     // Store state
     bool _isAboveSurface = false;
     bool _isFirstUpdate = true;
     readonly SampleHeightHelper _sampleHeightHelper = new SampleHeightHelper();
+
+    // Dynamic UnityEvent definitions.
+    [System.Serializable] public class FloatEvent : UnityEvent<float> { }
 
     void Update()
     {
@@ -33,7 +54,8 @@ public class OceanSampleHeightEvents : MonoBehaviour
 
         if (_sampleHeightHelper.Sample(out var height))
         {
-            var isAboveSurface = (transform.position.y - height) > 0;
+            var distance = transform.position.y - height;
+            var isAboveSurface = distance > 0;
 
             // Has the below/above ocean surface state changed?
             if (_isAboveSurface != isAboveSurface || _isFirstUpdate)
@@ -50,6 +72,21 @@ public class OceanSampleHeightEvents : MonoBehaviour
                     _onBelowOceanSurface.Invoke();
                 }
             }
+
+            // Save some processing when not being used.
+            if (_distanceFromOceanSurface.GetPersistentEventCount() > 0)
+            {
+                // Normalise distance so we can use the curve.
+                var distanceFromOceanSurface = _distanceCurve.Evaluate(1f - Mathf.Abs(distance) / _maximumDistance);
+
+                // Restore raw distance if desired.
+                if (!_normaliseDistance)
+                {
+                    distanceFromOceanSurface = _maximumDistance - distanceFromOceanSurface * _maximumDistance;
+                }
+
+                _distanceFromOceanSurface.Invoke(distanceFromOceanSurface);
+            }
         }
     }
 
@@ -62,8 +99,10 @@ public class OceanSampleHeightEvents : MonoBehaviour
             EditorGUILayout.Space();
             EditorGUILayout.HelpBox
             (
-                "Whenever this game object goes below or above the ocean surface, the appropriate event is fired " +
-                "once per state change. It can be used to trigger audio to play underwater and much more.",
+                "For the Above/Below Ocean Surface Events, whenever this game object goes below or above the ocean " + 
+                "surface, the appropriate event is fired once per state change. It can be used to trigger audio to " +
+                "play underwater and much more. For the Distance From Ocean Surface event, it will pass the " +
+                "distance every frame (passing normalised distance to audio volume as an example).",
                 MessageType.Info
             );
             EditorGUILayout.Space();

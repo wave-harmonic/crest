8e3988d0d1d3e20d36417b79646a3e3352bebe01
Huw Bowles <huw.bowles@gmail.com>
2021-01-22 11:50:09 +0000

    Unify and document cases for adding spline points

--- crest/Assets/Crest/Crest/Scripts/Spline/SplinePoint.cs
+++ crest/Assets/Crest/Crest/Scripts/Spline/SplinePoint.cs
@@ -95,20 +95,7 @@ namespace Crest.Spline
             label = thisIdx == 0 ? "Add before (extend)" : "Add before";
             if (GUILayout.Button(label))
             {
-                var newPoint = CreateNewSP();
-                newPoint.transform.parent = parent;
-                var newIdx = thisIdx;
-                newPoint.transform.SetSiblingIndex(newIdx);
-
-                if (thisIdx > 0)
-                {
-                    var beforeNewPoint = parent.GetChild(thisIdx - 1);
-                    newPoint.transform.position = Vector3.Lerp(beforeNewPoint.position, thisSP.transform.position, 0.5f);
-                }
-                else if (parent.childCount > 2)
-                {
-                    newPoint.transform.position = 2f * parent.GetChild(newIdx + 1).position - parent.GetChild(newIdx + 2).position;
-                }
+                var newPoint = AddSplinePointBefore(parent, thisIdx);
 
                 Undo.RegisterCreatedObjectUndo(newPoint, "Add Crest Spline Point");
 
@@ -132,9 +119,18 @@ namespace Crest.Spline
                 {
                     Selection.activeObject = parent.GetChild(thisIdx - 1);
                 }
-                else if (parent.childCount > 1)
+                else
                 {
-                    Selection.activeObject = parent.GetChild(1);
+                    // If there is more than one child, select the first
+                    if (parent.childCount > 1)
+                    {
+                        Selection.activeObject = parent.GetChild(1);
+                    }
+                    else
+                    {
+                        // No children - select the parent
+                        Selection.activeObject = parent;
+                    }
                 }
                 DestroyImmediate(thisSP.gameObject);
             }
@@ -148,8 +144,47 @@ namespace Crest.Spline
             return newPoint;
         }
 
+        public static GameObject AddSplinePointBefore(Transform parent, int beforeIdx = 0)
+        {
+            var newPoint = CreateNewSP();
+            newPoint.transform.parent = parent;
+
+            // Put in front of child at beforeIdx
+            newPoint.transform.SetSiblingIndex(beforeIdx);
+            // Inserting has moved the before point forwards, update its index to simplify the below
+            beforeIdx++;
+
+            if (parent.childCount == 1)
+            {
+                // New point is sole point, place at center
+                newPoint.transform.localPosition = Vector3.zero;
+            }
+            else if (parent.childCount == 2)
+            {
+                // New point has one sibling, place nearby it
+                newPoint.transform.position = parent.GetChild(beforeIdx).position - 10f * Vector3.forward;
+            }
+            else if (beforeIdx > 1)
+            {
+                // New point being inserted between two existing points, bisect them
+                var beforeNewPoint = parent.GetChild(beforeIdx);
+                var afterNewPoint = parent.GetChild(beforeIdx - 2);
+                newPoint.transform.position = Vector3.Lerp(beforeNewPoint.position, afterNewPoint.position, 0.5f);
+            }
+            else
+            {
+                // New point being inserted before first point, and spline has multiple points, extrapolate backwards
+                var newPos = 2f * parent.GetChild(1).position - parent.GetChild(2).position;
+                Debug.Log(newPos);
+                newPoint.transform.position = newPos;
+            }
+
+            return newPoint;
+        }
+
         public static GameObject AddSplinePointAfter(Transform parent, int afterIdx = -1)
         {
+            // If no index specified, assume adding after last point
             if (afterIdx == -1) afterIdx = parent.childCount - 1;
 
             var newPoint = CreateNewSP();
@@ -160,20 +195,24 @@ namespace Crest.Spline
 
             if (parent.childCount == 1)
             {
+                // New point is sole point, place at center
                 newPoint.transform.localPosition = Vector3.zero;
             }
             else if (parent.childCount == 2)
             {
+                // New point has one sibling, place nearby it
                 newPoint.transform.position = parent.GetChild(afterIdx).position + 10f * Vector3.forward;
             }
             else if (newIdx < parent.childCount - 1)
             {
+                // New point being inserted between two existing points, bisect them
                 var beforeNewPoint = parent.GetChild(afterIdx);
                 var afterNewPoint = parent.GetChild(afterIdx + 2);
                 newPoint.transform.position = Vector3.Lerp(beforeNewPoint.position, afterNewPoint.position, 0.5f);
             }
-            else if (parent.childCount > 2)
+            else
             {
+                // New point being added after last point, and spline has multiple points, extrapolate forwards
                 newPoint.transform.position = 2f * parent.GetChild(newIdx - 1).position - parent.GetChild(newIdx - 2).position;
             }
 

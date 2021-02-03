a13c4c5fb31739304caae972c7b2170fb22914dd
Dale Eidd <dale@eidd.name>
2021-02-03 14:06:24 -0800

    Cleanup
    AttributeUsage already inherited...

--- crest/Assets/Crest/Crest/Scripts/Helpers/Attributes/EmbeddedFieldAttribute.cs
+++ crest/Assets/Crest/Crest/Scripts/Helpers/Attributes/EmbeddedFieldAttribute.cs
@@ -1,9 +1,12 @@
-﻿namespace Crest
-{
-    using UnityEngine;
-    using Crest.EditorHelpers;
+﻿// Crest Ocean System
 
-    [System.AttributeUsage(System.AttributeTargets.Field)]
+// This file is subject to the MIT License as seen in the root of this folder structure (LICENSE)
+
+namespace Crest
+{
+    using Crest.EditorHelpers;
+    using UnityEngine;
+
     public class EmbeddedFieldAttribute : PropertyAttribute
     {
         internal EmbeddeAssetEditor editor;

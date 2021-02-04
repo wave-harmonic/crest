6ddf1e92482f2cf92ced81d19820d2796af45c20
Dale Eidd <dale@eidd.name>
2021-02-03 16:14:26 -0800

    Remove gap above embedded inspector

--- crest/Assets/Crest/Crest/Scripts/Helpers/Editor/EmbeddedAttributeEditor.cs
+++ crest/Assets/Crest/Crest/Scripts/Helpers/Editor/EmbeddedAttributeEditor.cs
@@ -11,5 +11,8 @@
             EmbeddedFieldAttribute embeddedAttribute = (EmbeddedFieldAttribute)attribute;
             embeddedAttribute.editor.DrawEditorCombo(this, property, "asset");
         }
+
+        // Removes space above embedded editor so the embedded editor replaces this drawer.
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label) => 0f;
     }
 }

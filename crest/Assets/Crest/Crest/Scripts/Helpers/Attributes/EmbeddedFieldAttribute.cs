dec36c61f76fa97ab900f7a4fbf20bcc2ff8760e
Dale Eidd <dale@eidd.name>
2021-02-03 16:21:05 -0800

    Move EmbeddedFieldAttributePropertyDrawer

--- crest/Assets/Crest/Crest/Scripts/Helpers/Attributes/EmbeddedFieldAttribute.cs
+++ crest/Assets/Crest/Crest/Scripts/Helpers/Attributes/EmbeddedFieldAttribute.cs
@@ -7,6 +7,10 @@ namespace Crest
     using Crest.EditorHelpers;
     using UnityEngine;
 
+#if UNITY_EDITOR
+    using UnityEditor;
+#endif
+
     public class EmbeddedFieldAttribute : PropertyAttribute
     {
         internal EmbeddedAssetEditor editor;
@@ -16,4 +20,19 @@ namespace Crest
             editor = new EmbeddedAssetEditor();
         }
     }
+
+#if UNITY_EDITOR
+    [CustomPropertyDrawer(typeof(EmbeddedFieldAttribute))]
+    public class EmbeddedFieldAttributePropertyDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EmbeddedFieldAttribute embeddedAttribute = (EmbeddedFieldAttribute)attribute;
+            embeddedAttribute.editor.DrawEditorCombo(this, property, "asset");
+        }
+
+        // Removes space above embedded editor so the embedded editor replaces this drawer.
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label) => 0f;
+    }
+#endif
 }

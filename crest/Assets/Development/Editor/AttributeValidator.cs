4640100f6ded446a1a112f63d44258dc8edb0444
Dale Eidd <dale@eidd.name>
2021-04-16 17:42:37 -0700

    Add decorator validator
    Decorator attributes alone will silently fail. Log an error if they
    don't have a decorated attribute.

---
+++ crest/Assets/Development/Editor/AttributeValidator.cs
@@ -0,0 +1,37 @@
+﻿// Crest Ocean System
+
+// This file is subject to the MIT License as seen in the root of this folder structure (LICENSE)
+
+namespace Crest
+{
+    using System.Reflection;
+    using UnityEditor.Callbacks;
+    using System.Linq;
+    using UnityEngine;
+    using System;
+
+    /// <summary>
+    /// Validates decorator attributes. They need to also have a decorated attribute to work.
+    /// </summary>
+    static class AttributeValidator
+    {
+        [DidReloadScripts]
+        static void OnDidReloadScripts()
+        {
+            // TODO: Try using TypeCache in Unity 2020.
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies().Where(x => x.FullName.StartsWith("Crest")).ToList();
+            foreach (var assembly in assemblies)
+            {
+                foreach (var type in assembly.GetTypes())
+                {
+                    foreach (var property in type.GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public))
+                    {
+                        if (property.GetCustomAttribute<DecoratorAttribute>() == null) continue;
+                        if (property.GetCustomAttribute<DecoratedPropertyAttribute>() != null) continue;
+                        Debug.LogError($"A decorator attribute on {type}.{property.Name} has no attribute which inherits from DecoratedPropertyAttribute. The decorator will be ignored.");
+                    }
+                }
+            }
+        }
+    }
+}

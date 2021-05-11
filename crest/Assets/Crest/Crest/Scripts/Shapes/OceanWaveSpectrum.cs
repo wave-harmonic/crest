1f7969b8c3483674698ae5f0d25442a2b8cf2249
Huw Bowles <huw.bowles@gmail.com>
2021-05-11 06:40:24 +0100

    Fix comment

--- crest/Assets/Crest/Crest/Scripts/Shapes/OceanWaveSpectrum.cs
+++ crest/Assets/Crest/Crest/Scripts/Shapes/OceanWaveSpectrum.cs
@@ -148,7 +148,7 @@ namespace Crest
 
             var c = ComputeWaveSpeed(wavelength);
 
-            // Dampen based on wind. Waves travelling faster than wind get dampened. 0.95
+            // Dampen based on wind. Waves travelling faster than wind get dampened. 0.75
             // is arbitrary value to give some 'ramp' in the multiplier.
             power *= Mathf.SmoothStep(0f, 1f, Mathf.InverseLerp(windSpeed, windSpeed * 0.75f, c));
             var a_2 = 2f * power * domega;

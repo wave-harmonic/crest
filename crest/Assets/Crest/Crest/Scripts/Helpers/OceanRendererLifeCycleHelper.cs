a5fd2ca3d227b14ca5740906194f9be658177d49
Dale Eidd <dale@eidd.name>
2021-07-15 00:53:51 -0700

    Made robust and added comments

--- crest/Assets/Crest/Crest/Scripts/Helpers/OceanRendererLifeCycleHelper.cs
+++ crest/Assets/Crest/Crest/Scripts/Helpers/OceanRendererLifeCycleHelper.cs
@@ -2,31 +2,50 @@
 
 // This file is subject to the MIT License as seen in the root of this folder structure (LICENSE)
 
-using UnityEngine;
-
 namespace Crest
 {
+    using UnityEngine;
+
     /// <summary>
-    /// Enables/disables a component based on the OceanRenderer's life cycle.
+    /// Enables/disables a component based on the OceanRenderer's life cycle. It will allow a component to initialise
+    /// after the OceanRenderer has initialised (i.e. delayed initialisation).
+    ///
+    /// It stores the "enabled" state set by the user as it enables or disables the component if the OceanRenderer is
+    /// enabled or disabled respectively using event subscription broadcasted from the OceanRenderer.
+    ///
+    /// It supports [ExecuteAlways/InEditMode] by storing the "enabled" state, but does not enable or disable the
+    /// component as that would change serialized data.
+    ///
+    /// Components must call OnEnable, OnDisable and OnDestroy from their MonoBehaviour counterparts.
     /// </summary>
     public class OceanRendererLifeCycleHelper
     {
         // The mono behaviour we want to track the enabled/disabled preference.
         readonly MonoBehaviour _monoBehaviour;
-        // Store the enabled/disabled checkbox set by the user so we can respect it.
-        bool _enabledUserPreference;
-        // Whether the state change came from the user or from the ocean life cycle event.
-        bool _isStateChangeFromUser = true;
+
+        // Store the "enabled" state set by the user so we can respect it.
+        internal bool _enabledValueSetByTheUser;
+
+        // Whether the state change came from the user or from the ocean life cycle event. Generally, this should always
+        // be true since state changes from users are unknown.
+        bool _isStateChangeFromUser = false;
 
         public OceanRendererLifeCycleHelper(MonoBehaviour monoBehaviour)
         {
             _monoBehaviour = monoBehaviour;
-            _enabledUserPreference = _monoBehaviour.enabled;
+            // Store the "enabled" state set by the user.
+            _enabledValueSetByTheUser = _monoBehaviour.enabled;
 
             OceanRenderer.OnOceanRendererEnabled -= OnOceanRendererEnabled;
             OceanRenderer.OnOceanRendererEnabled += OnOceanRendererEnabled;
             OceanRenderer.OnOceanRendererDisabled -= OnOceanRendererDisabled;
             OceanRenderer.OnOceanRendererDisabled += OnOceanRendererDisabled;
+        }
+
+        ~OceanRendererLifeCycleHelper()
+        {
+            OceanRenderer.OnOceanRendererEnabled -= OnOceanRendererEnabled;
+            OceanRenderer.OnOceanRendererDisabled -= OnOceanRendererDisabled;
         }
 
         /// <summary>
@@ -43,28 +62,33 @@ namespace Crest
         /// </summary>
         public bool OnEnable()
         {
+#if UNITY_EDITOR
+            if (!Application.isPlaying)
+            {
+                // In edit mode just store "enabled" as we do not disable the component anyway.
+                _enabledValueSetByTheUser = _monoBehaviour.enabled;
+                return OceanRenderer.Instance != null;
+            }
+#endif
+
             if (_isStateChangeFromUser)
             {
-                _enabledUserPreference = true;
+                // Flip value as it could be either the user enabling or disabling the component. For the case where the
+                // ocean renderer is not active and this component is set to be enabled, the component will be disabled.
+                // The user will see an unchecked checkbox for "enable" as they should. When they toggle it, it will
+                // enable the component but since the ocean is disabled it will disable itself after recording the
+                // user's preference.
+                _enabledValueSetByTheUser =  !_enabledValueSetByTheUser;
             }
-            else
-            {
-                // This change came from elsewhere. We must always clear this after a state change from elsewhere.
-                _isStateChangeFromUser = true;
-            }
+
+            // Always revert to "true" as whether the change comes from a user is unknown.
+            _isStateChangeFromUser = true;
 
             if (OceanRenderer.Instance == null)
             {
-                // False, because we do not want OnDisable changing the preference.
+                // Mark "false" as we will now disable the component and the subsequent OnDisable is not from the user.
                 _isStateChangeFromUser = false;
-#if UNITY_EDITOR
-                // We do not want to change a serialised value in edit mode.
-                if (Application.isPlaying)
-#endif
-                {
-                    _monoBehaviour.enabled = false;
-                }
-
+                _monoBehaviour.enabled = false;
                 return false;
             }
 
@@ -76,42 +100,54 @@ namespace Crest
         /// </summary>
         public void OnDisable()
         {
+#if UNITY_EDITOR
+            if (!Application.isPlaying)
+            {
+                // In edit mode just store "enabled" as we do not disable the component anyway.
+                _enabledValueSetByTheUser = _monoBehaviour.enabled;
+                return;
+            }
+#endif
+
             if (_isStateChangeFromUser)
             {
-                _enabledUserPreference = false;
+                _enabledValueSetByTheUser = false;
             }
-            else
-            {
-                // This change came from elsewhere. We must always clear this after a state change from elsewhere.
-                _isStateChangeFromUser = true;
-            }
+
+            // Always revert to "true" as whether the change comes from a user is unknown.
+            _isStateChangeFromUser = true;
         }
 
+        // This will be called by the OceanRenderer through an event. If during the component's OnEnable event the
+        // OceanRenderer is already ready, then this will be skipped.
         void OnOceanRendererEnabled()
         {
-            // State change is from here which is not the user. Flag it so we know not to update the user preference.
-            _isStateChangeFromUser = !_enabledUserPreference;
-
 #if UNITY_EDITOR
             // We do not want to change a serialised value in edit mode.
-            if (Application.isPlaying)
-#endif
+            if (!Application.isPlaying)
             {
-                _monoBehaviour.enabled = _enabledUserPreference;
+                return;
             }
+#endif
+
+            // If these two are equal then this event will do nothing and next OnEnable/OnDisable could be the user.
+            _isStateChangeFromUser = _monoBehaviour.enabled == _enabledValueSetByTheUser;
+            // Apply the "enabled" state that the user wants now that OceanRenderer is ready.
+            _monoBehaviour.enabled = _enabledValueSetByTheUser;
         }
 
+        // This will be called by the OceanRenderer through an event.
         void OnOceanRendererDisabled()
         {
-            // State change is from here which is not the user. Flag it so we know not to update the user preference.
-            _isStateChangeFromUser = !_enabledUserPreference;
-#if UNITY_EDITOR
-            // We do not want to change a serialised value in edit mode.
-            if (Application.isPlaying)
-#endif
+            if (!Application.isPlaying)
             {
-                _monoBehaviour.enabled = false;
+                return;
             }
+
+            // If the user has set the component to disabled, then this event will do nothing and next
+            // OnEnable/OnDisable could be the user.
+            _isStateChangeFromUser = _enabledValueSetByTheUser == false;
+            _monoBehaviour.enabled = false;
         }
     }
 }

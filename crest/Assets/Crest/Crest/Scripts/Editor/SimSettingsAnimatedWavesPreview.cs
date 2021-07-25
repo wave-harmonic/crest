35ba71f69211d5613460ed1009a05837d24917f9
Huw Bowles <huw.bowles@gmail.com>
2021-07-25 12:37:25 +0100

    Minor header

--- crest/Assets/Crest/Crest/Scripts/Editor/SimSettingsAnimatedWavesPreview.cs
+++ crest/Assets/Crest/Crest/Scripts/Editor/SimSettingsAnimatedWavesPreview.cs
@@ -1,3 +1,7 @@
+// Crest Ocean System
+
+// This file is subject to the MIT License as seen in the root of this folder structure (LICENSE)
+
 using Unity.Collections;
 using UnityEditor;
 using UnityEngine;

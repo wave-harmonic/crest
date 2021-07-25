49a1e112ef7dca3cbe0283080545fd5c30977620
Huw Bowles <huw.bowles@gmail.com>
2021-07-25 22:07:03 +0100

    Missed cleanup on preview

--- crest/Assets/Crest/Crest/Scripts/Editor/SimSettingsAnimatedWavesPreview.cs
+++ crest/Assets/Crest/Crest/Scripts/Editor/SimSettingsAnimatedWavesPreview.cs
@@ -8,6 +8,9 @@ using UnityEngine;
 
 namespace Crest
 {
+    /// <summary>
+    /// If the collision source in the animated wave settings is set to baked FFT, shows a preview of the data.
+    /// </summary>
     [CustomPreview(typeof(SimSettingsAnimatedWaves))]
     public class SimSettingsAnimatedWavesPreview : ObjectPreview
     {
@@ -16,46 +19,56 @@ namespace Crest
         private Object _previousTarget;
         private SimSettingsAnimatedWaves _targetAnimatedWaves => target as SimSettingsAnimatedWaves;
         private FFTBakedData _targetBakedData => _targetAnimatedWaves?._bakedFFTData;
-    
+
         public override bool HasPreviewGUI()
         {
             return _targetAnimatedWaves.CollisionSource == SimSettingsAnimatedWaves.CollisionSources.BakedFFT && _targetBakedData != null;
         }
-    
+
+        /// <summary>
+        /// Tries to draw a GUI in the preview header bar (frame selector). Behaviour is a bit funky but
+        /// hopefully ok.
+        /// </summary>
         public override void OnPreviewSettings()
         {
             EditorGUILayout.BeginHorizontal(GUILayout.Width(30f));
             GUILayout.Label("Frame");
-    
+
             var lastFrame = _targetBakedData != null ? _targetBakedData._parameters._frameCount - 1 : 0;
             _frameToPreview = EditorGUILayout.IntSlider(_frameToPreview, 0, lastFrame);
             EditorGUILayout.EndHorizontal();
         }
-    
+
+        /// <summary>
+        /// Text displayed on top of preview.
+        /// </summary>
         public override string GetInfoString()
         {
             var data = _targetBakedData;
             if (data == null) return "";
-            return $"{data.name}, {data._parameters._textureResolution}x({data._parameters._textureResolution}x{data._parameters._lodCount}), {_frameToPreview+1}/{data._parameters._frameCount}";
+            return $"{data.name}, {data._parameters._textureResolution}x({data._parameters._textureResolution}x{data._parameters._lodCount}), {_frameToPreview + 1}/{data._parameters._frameCount}";
         }
-    
+
+        /// <summary>
+        /// Draws selected baked data frame.
+        /// </summary>
         public override void OnInteractivePreviewGUI(Rect r, GUIStyle background)
         {
             base.OnPreviewGUI(r, background);
-    
+
             if (_targetBakedData == null)
                 return;
-    
+
             if (Mathf.Approximately(r.width, 1f))
                 return;
-    
+
             if (target != _previousTarget)
             {
                 var allFramesAllPixels = _targetBakedData._framesFlattenedNative;
                 var singleFrameSize = allFramesAllPixels.Length / _targetBakedData._parameters._frameCount;
 
                 var rawDataNative = new NativeArray<float>(singleFrameSize * 4, Allocator.Temp);
-                
+
                 for (int i = 0; i < singleFrameSize / FFTBakedData.kFloatsPerPoint; i++)
                 {
                     // map to 0-1 range for visualisation purposes
@@ -68,7 +81,7 @@ namespace Crest
                     var mappedValueX = Mathf.Lerp(-6f, 6f, alphaX);
                     var mappedValueY = Mathf.Lerp(-6f, 6f, alphaY);
                     var mappedValueZ = Mathf.Lerp(-6f, 6f, alphaZ);
-    
+
                     // convert to grayscale (if there's a prettier way, feel free)
                     var nativeIndex = i * 4;
                     rawDataNative[nativeIndex + 0] = Mathf.Clamp01(mappedValueX);
@@ -79,20 +92,19 @@ namespace Crest
 
                 if (rawDataNative.Length == 0)
                     return;
-    
+
                 _previewTexture ??= new Texture2D(
                     _targetBakedData._parameters._textureResolution,
                     _targetBakedData._parameters._textureResolution * _targetBakedData._parameters._lodCount,
                     TextureFormat.RGBAFloat, false, true);
-    
+
                 _previewTexture.LoadRawTextureData(rawDataNative);
                 _previewTexture.Apply();
 
                 rawDataNative.Dispose();
             }
-    
+
             GUI.DrawTexture(r, _previewTexture, ScaleMode.ScaleToFit, false);
-            // targetAnimatedWaves._bakedDataFrameToPreview = (_frameToPreview + 1) % targetBakedData._frameCount;
         }
     }
 }

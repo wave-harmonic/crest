cbd039eed6cfe81b46c6ef517d57c46b6e8c97ef
Huw Bowles <huw.bowles@gmail.com>
2021-09-18 07:16:12 +0100

    Inline sea floor depth settings

--- crest/Assets/Crest/Crest/Scripts/OceanRenderer.cs
+++ crest/Assets/Crest/Crest/Scripts/OceanRenderer.cs
@@ -212,6 +212,7 @@ namespace Crest
         [Tooltip("Water depth information used for shallow water, shoreline foam, wave attenuation, among others."), SerializeField]
         bool _createSeaFloorDepthData = true;
         public bool CreateSeaFloorDepthData { get { return _createSeaFloorDepthData; } }
+        [Predicated("_createSeaFloorDepthData"), Embedded]
         public SimSettingsSeaFloorDepth _simSettingsSeaFloorDepth;
 
         [Tooltip("Simulation of foam created in choppy water and dissipating over time."), SerializeField]

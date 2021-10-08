220ca9e8c18b418f31b6e7d47ab93d6f80e13d8c
Dale Eidd <dale@eidd.name>
2021-10-08 15:31:28 -0700

    Fix shader error

--- crest/Assets/Crest/Crest/Shaders/OceanHelpersNew.hlsl
+++ crest/Assets/Crest/Crest/Shaders/OceanHelpersNew.hlsl
@@ -111,9 +111,9 @@ void SampleSeaDepth(in Texture2DArray i_oceanDepthSampler, in float3 i_uv_slice,
 	io_oceanDepth += i_wt * (waterDepth - CREST_OCEAN_DEPTH_BASELINE);
 }
 
-void SampleSingleSeaDepth(Texture2DArray i_texture, float3 i_uv, inout half io_seaDepth, inout half io_seaLevelOffset)
+void SampleSingleSeaDepth(Texture2DArray i_oceanDepthSampler, float3 i_uv_slice, inout half io_oceanDepth, inout half io_seaLevelOffset)
 {
-	const half2 terrainHeight_seaLevelOffset = i_texture.SampleLevel(LODData_linear_clamp_sampler, i_uv, 0.0).xy;
+	const half2 terrainHeight_seaLevelOffset = i_oceanDepthSampler.SampleLevel(LODData_linear_clamp_sampler, i_uv_slice, 0.0).xy;
 	const half waterDepth = _OceanCenterPosWorld.y - terrainHeight_seaLevelOffset.x + terrainHeight_seaLevelOffset.y;
 	io_oceanDepth = waterDepth - CREST_OCEAN_DEPTH_BASELINE;
 	io_seaLevelOffset = terrainHeight_seaLevelOffset.y;

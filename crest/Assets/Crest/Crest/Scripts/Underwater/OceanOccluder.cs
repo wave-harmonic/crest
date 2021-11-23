ff7c384e2e53b4a749c7f6a34ebfe28cf6ee71c7
Dale Eidd <dale@eidd.name>
2021-11-23 04:27:39 -0800

    Fix null exception

--- crest/Assets/Crest/Crest/Scripts/Underwater/OceanOccluder.cs
+++ crest/Assets/Crest/Crest/Scripts/Underwater/OceanOccluder.cs
@@ -84,8 +84,7 @@ namespace Crest
                 material.SetTexture("_SurfaceNormal", OceanRenderer.Instance.OceanMaterial.GetTexture("_Normals"));
             }
 
-            // var underwater = _currentCamera.GetComponent<IUnderwaterPostProcessPerCameraData>();
-            // if (underwater != null && underwater.enabled)
+            if (UnderwaterRenderer.Instance != null)
             {
                 // TODO: Move registry to this component?
                 UnderwaterRenderer.Instance.RegisterOceanOccluder(this);

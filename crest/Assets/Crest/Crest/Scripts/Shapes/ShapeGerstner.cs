c41b26966554d3e75ddb3c57f5e851f3f40d97d5
Dale Eidd <dale@eidd.name>
2022-10-19 14:59:47 +0800

    Remove unused imports

--- crest/Assets/Crest/Crest/Scripts/Shapes/ShapeGerstner.cs
+++ crest/Assets/Crest/Crest/Scripts/Shapes/ShapeGerstner.cs
@@ -10,11 +10,6 @@ using Crest.Spline;
 
 #if UNITY_EDITOR
 using UnityEditor;
-#if UNITY_2021_2_OR_NEWER
-using UnityEditor.SceneManagement;
-#else
-using UnityEditor.Experimental.SceneManagement;
-#endif
 #endif
 
 namespace Crest

868ecb2227888b5ec7f0e88945529f3036a64dbc
Dale Eidd <dale@eidd.name>
2024-02-02 18:22:47 +0800

    Simplify null providers

--- crest/Assets/Crest/Crest/Scripts/Collision/CollProviderNull.cs
+++ crest/Assets/Crest/Crest/Scripts/Collision/CollProviderNull.cs
@@ -14,70 +14,38 @@ namespace Crest
     {
 #if CREST_BURST_QUERY
         public int Query(int i_ownerHash, float i_minSpatialLength, ref NativeArray<Vector3> i_queryPoints, ref NativeArray<Vector3> o_resultDisps, ref NativeArray<Vector3> o_resultNorms, ref NativeArray<Vector3> o_resultVels, bool useNormals)
+        {
+            for (int i = 0; i < o_resultDisps.Length; i++) o_resultDisps[i] = Vector3.zero;
+            for (int i = 0; i < o_resultNorms.Length; i++) o_resultNorms[i] = Vector3.up;
+            for (int i = 0; i < o_resultVels.Length; i++) o_resultVels[i] = Vector3.zero;
+            return 0;
+        }
+
+        public int Query(int i_ownerHash, float i_minSpatialLength, ref NativeArray<Vector3> i_queryPoints, ref NativeArray<float> o_resultHeights, ref NativeArray<Vector3> o_resultNorms, ref NativeArray<Vector3> o_resultVels)
+        {
+            for (int i = 0; i < o_resultHeights.Length; i++) o_resultHeights[i] = 0f;
+            for (int i = 0; i < o_resultNorms.Length; i++) o_resultNorms[i] = Vector3.up;
+            for (int i = 0; i < o_resultVels.Length; i++) o_resultVels[i] = Vector3.zero;
+            return 0;
+        }
 #else
         public int Query(int i_ownerHash, float i_minSpatialLength, Vector3[] i_queryPoints, Vector3[] o_resultDisps, Vector3[] o_resultNorms, Vector3[] o_resultVels)
-#endif
+
         {
-            if (o_resultDisps != null)
-            {
-                for (int i = 0; i < o_resultDisps.Length; i++)
-                {
-                    o_resultDisps[i] = Vector3.zero;
-                }
-            }
-
-            if (o_resultNorms != null)
-            {
-                for (int i = 0; i < o_resultNorms.Length; i++)
-                {
-                    o_resultNorms[i] = Vector3.up;
-                }
-            }
-
-            if (o_resultVels != null)
-            {
-                for (int i = 0; i < o_resultVels.Length; i++)
-                {
-                    o_resultVels[i] = Vector3.zero;
-                }
-            }
-
+            for (int i = 0; i < o_resultDisps?.Length; i++) o_resultDisps[i] = Vector3.zero;
+            for (int i = 0; i < o_resultNorms?.Length; i++) o_resultNorms[i] = Vector3.up;
+            for (int i = 0; i < o_resultVels?.Length; i++) o_resultVels[i] = Vector3.zero;
             return 0;
         }
-
-#if CREST_BURST_QUERY
-        public int Query(int i_ownerHash, float i_minSpatialLength, ref NativeArray<Vector3> i_queryPoints, ref NativeArray<float> o_resultHeights, ref NativeArray<Vector3> o_resultNorms, ref NativeArray<Vector3> o_resultVels)
-#else
 
         public int Query(int i_ownerHash, float i_minSpatialLength, Vector3[] i_queryPoints, float[] o_resultHeights, Vector3[] o_resultNorms, Vector3[] o_resultVels)
-#endif
         {
-            if (o_resultHeights != null)
-            {
-                for (int i = 0; i < o_resultHeights.Length; i++)
-                {
-                    o_resultHeights[i] = 0f;
-                }
-            }
-
-            if (o_resultNorms != null)
-            {
-                for (int i = 0; i < o_resultNorms.Length; i++)
-                {
-                    o_resultNorms[i] = Vector3.up;
-                }
-            }
-
-            if (o_resultVels != null)
-            {
-                for (int i = 0; i < o_resultVels.Length; i++)
-                {
-                    o_resultVels[i] = Vector3.zero;
-                }
-            }
-
+            for (int i = 0; i < o_resultHeights?.Length; i++) o_resultHeights[i] = 0f;
+            for (int i = 0; i < o_resultNorms?.Length; i++) o_resultNorms[i] = Vector3.up;
+            for (int i = 0; i < o_resultVels?.Length; i++) o_resultVels[i] = Vector3.zero;
             return 0;
         }
+#endif
 
         public bool RetrieveSucceeded(int queryStatus)
         {

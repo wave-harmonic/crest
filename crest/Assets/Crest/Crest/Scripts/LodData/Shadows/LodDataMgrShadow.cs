9aa684833aa81d9f89125b43a235db0cf639fe2a
Dale Eidd <dale@eidd.name>
2024-04-18 19:10:49 +0800

    Remove random write requirement for shadows

--- crest/Assets/Crest/Crest/Scripts/LodData/Shadows/LodDataMgrShadow.cs
+++ crest/Assets/Crest/Crest/Scripts/LodData/Shadows/LodDataMgrShadow.cs
@@ -18,7 +18,7 @@ namespace Crest
     {
         public override string SimName => "Shadow";
         protected override GraphicsFormat RequestedTextureFormat => GraphicsFormat.R8G8_UNorm;
-        protected override bool NeedToReadWriteTextureData => true;
+        protected override bool NeedToReadWriteTextureData => false;
         static Texture2DArray s_nullTexture => TextureArrayHelpers.BlackTextureArray;
         protected override Texture2DArray NullTexture => s_nullTexture;
         public override int BufferCount => 2;

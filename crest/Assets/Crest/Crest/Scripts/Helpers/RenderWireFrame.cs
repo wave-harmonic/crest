e20403aba2057740c8cdb1277708c49f9566f121
Dale Eidd <dale@eidd.name>
2024-04-21 09:16:22 +0800

    Add missing namespace

--- crest/Assets/Crest/Crest/Scripts/Helpers/RenderWireFrame.cs
+++ crest/Assets/Crest/Crest/Scripts/Helpers/RenderWireFrame.cs
@@ -4,54 +4,57 @@
 
 using UnityEngine;
 
-/// <summary>
-/// Triggers the scene render to happen in wireframe.
-/// </summary>
-[RequireComponent(typeof(Camera))]
-[AddComponentMenu(Crest.Internal.Constants.MENU_PREFIX_DEBUG + "Render Wire Frame")]
-public class RenderWireFrame : MonoBehaviour
+namespace Crest
 {
-    public bool _enable;
-    public CameraClearFlags _clearFlags = CameraClearFlags.SolidColor;
-    public Color _backgroundColor = new Color(0.1921569f, 0.3019608f, 0.4745098f, 0f);
-    public LayerMask _cullingMask = ~0;
-    public static bool _wireFrame = false;
-
-    Camera _cam;
-    CameraClearFlags _oldClearFlags;
-    Color _oldBackgroundColor;
-    LayerMask _oldCullingMask;
-
-    public bool Active => _wireFrame || _enable;
-
-    void Start()
+    /// <summary>
+    /// Triggers the scene render to happen in wireframe.
+    /// </summary>
+    [RequireComponent(typeof(Camera))]
+    [AddComponentMenu(Crest.Internal.Constants.MENU_PREFIX_DEBUG + "Render Wire Frame")]
+    public class RenderWireFrame : MonoBehaviour
     {
-        _cam = GetComponent<Camera>();
-        _oldClearFlags = _cam.clearFlags;
-        _oldBackgroundColor = _cam.backgroundColor;
-        _oldCullingMask = _cam.cullingMask;
-    }
+        public bool _enable;
+        public CameraClearFlags _clearFlags = CameraClearFlags.SolidColor;
+        public Color _backgroundColor = new Color(0.1921569f, 0.3019608f, 0.4745098f, 0f);
+        public LayerMask _cullingMask = ~0;
+        public static bool _wireFrame = false;
 
-    void Update()
-    {
-        _cam.clearFlags = Active ? _clearFlags : _oldClearFlags;
-        _cam.backgroundColor = Active ? _backgroundColor : _oldBackgroundColor;
-        _cam.cullingMask = Active ? _cullingMask : _oldCullingMask;
-    }
+        Camera _cam;
+        CameraClearFlags _oldClearFlags;
+        Color _oldBackgroundColor;
+        LayerMask _oldCullingMask;
 
-    void OnPreRender()
-    {
-        if (enabled)
+        public bool Active => _wireFrame || _enable;
+
+        void Start()
         {
-            GL.wireframe = Active;
+            _cam = GetComponent<Camera>();
+            _oldClearFlags = _cam.clearFlags;
+            _oldBackgroundColor = _cam.backgroundColor;
+            _oldCullingMask = _cam.cullingMask;
         }
-    }
 
-    void OnPostRender()
-    {
-        if (enabled)
+        void Update()
         {
-            GL.wireframe = false;
+            _cam.clearFlags = Active ? _clearFlags : _oldClearFlags;
+            _cam.backgroundColor = Active ? _backgroundColor : _oldBackgroundColor;
+            _cam.cullingMask = Active ? _cullingMask : _oldCullingMask;
+        }
+
+        void OnPreRender()
+        {
+            if (enabled)
+            {
+                GL.wireframe = Active;
+            }
+        }
+
+        void OnPostRender()
+        {
+            if (enabled)
+            {
+                GL.wireframe = false;
+            }
         }
     }
 }
